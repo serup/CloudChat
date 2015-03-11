15690da8d2197f22ccc88f91f2b60403324f61af
Johnny Serup <serup@hotmail.com>
2015-03-11 16:55:25 +0100

    bugfix

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-#cd codeblocks_projects
-#make -f makefile_ubuntu.mak total 
-#cd ../javascript_projects
-cd javascript_projects
+cd codeblocks_projects
+make -f makefile_ubuntu.mak total 
+cd ../javascript_projects
 make -f makefile_ubuntu.mak total
