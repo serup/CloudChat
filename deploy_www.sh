ba2567798a15b71eee320e40e9161648b3a5e3d4
Johnny Serup <serup@hotmail.com>
2015-03-13 12:52:23 +0100

    bugfix

--- deploy_www.sh
+++ deploy_www.sh
@@ -1,7 +1,14 @@
+rm -rf /var/www/DOPS
+mkdir -p /var/www/DOPS/CloudChat/CloudChatManager
+mkdir -p /var/www/DOPS/CloudChat/CloudChatClient
+mkdir -p /var/www/DOPS/DataEncoderDecoder
+mkdir -p /var/www/DOPS/socketserverAPI
 rm -rf /var/scanva/release/master/
 cd /var/scanva/release/
 mkdir master
 cd master
 git clone -b master https://github.com/serup/CloudChat.git
-cp -r /var/scanva/release/master/CloudChat/javascript_projects/CloudChat/CloudChatManager /var/www/DOPS/CloudChat/CloudChatManager
-cp -r /var/scanva/release/master/CloudChat/javascript_projects/CloudChat/CloudChatClient /var/www/DOPS/CloudChat/CloudChatClient
+cp -r /var/scanva/release/master/CloudChat/javascript_projects/CloudChat/CloudChatManager /var/www/DOPS/CloudChat
+cp -r /var/scanva/release/master/CloudChat/javascript_projects/CloudChat/CloudChatClient /var/www/DOPS/CloudChat
+cp -r /var/scanva/release/master/CloudChat/javascript_projects/DataEncoderDecoder /var/www/DOPS
+cp -r /var/scanva/release/master/CloudChat/javascript_projects/socketserverAPI /var/www/DOPS
