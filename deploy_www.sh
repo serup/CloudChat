a8750b78406661d98ffee3e8608053cb74abc47a
Johnny Serup <serup@hotmail.com>
2015-03-13 12:03:56 +0100

    success - deploy to www of cloudchatmanager

--- deploy_www.sh
+++ deploy_www.sh
@@ -1,2 +1,6 @@
-cd /var/www/DOPS
+rm -rf /var/scanva/release/master/
+cd /var/scanva/release/
+mkdir master
+cd master
 git clone -b master https://github.com/serup/CloudChat.git
+cp -r /var/scanva/release/master/CloudChat/javascript_projects/CloudChat/CloudChatManager /var/www/DOPS/CloudChat/CloudChatManager
