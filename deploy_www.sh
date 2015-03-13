06b6f84bde705244f61d65125081090ddefa331e
Johnny Serup <serup@hotmail.com>
2015-03-13 12:06:32 +0100

    boble - add cloudchatclient to deploy job

--- deploy_www.sh
+++ deploy_www.sh
@@ -4,3 +4,4 @@ mkdir master
 cd master
 git clone -b master https://github.com/serup/CloudChat.git
 cp -r /var/scanva/release/master/CloudChat/javascript_projects/CloudChat/CloudChatManager /var/www/DOPS/CloudChat/CloudChatManager
+cp -r /var/scanva/release/master/CloudChat/javascript_projects/CloudChat/CloudChatClient /var/www/DOPS/CloudChat/CloudChatClient
