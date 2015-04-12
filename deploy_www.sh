69e1aa6a699c595b78a41d6e87b313d3e2d7ef52
Johnny Serup <johnny.serup@scanva.com>
2015-04-12 14:01:32 +0200

    boble : still work on deploy script
    Change-Id: I863b0846970a1b79bbcbebd9b83eee093c30b11f

--- deploy_www.sh
+++ deploy_www.sh
@@ -3,18 +3,18 @@ mkdir -p /var/www/DOPS/CloudChat/CloudChatManager
 mkdir -p /var/www/DOPS/CloudChat/CloudChatClient
 mkdir -p /var/www/DOPS/DataEncoderDecoder
 mkdir -p /var/www/DOPS/socketserverAPI
-rm -rf /var/scanva/release/serup/
-cd /var/scanva/release/
+#rm -rf /var/scanva/release/serup/
+#cd /var/scanva/release/
 #mkdir serup 
-cd serup 
+#cd serup 
 #git clone -b serup ssh://serup@review.gerrithub.io:29418/serup/CloudChat
-cd CloudChat
+#cd CloudChat
 #./run.sh
-cp -r /var/scanva/release/serup/CloudChat/javascript_projects/CloudChat/CloudChatManager /var/www/DOPS/CloudChat
-cp -r /var/scanva/release/serup/CloudChat/javascript_projects/CloudChat/CloudChatClient /var/www/DOPS/CloudChat
-cp -r /var/scanva/release/serup/CloudChat/javascript_projects/DataEncoderDecoder /var/www/DOPS
-cp -r /var/scanva/release/serup/CloudChat/javascript_projects/socketserverAPI /var/www/DOPS
-cp -r /var/scanva/release/serup/CloudChat/codeblocks_projects/DataDictionary /var/www/DOPS
-cd codeblocks_projects
+cp -r /var/lib/jenkins/workspace/GerritCloudChat serup branch/javascript_projects/CloudChat/CloudChatManager /var/www/DOPS/CloudChat
+cp -r /var/lib/jenkins/workspace/GerritCloudChat serup branch/javascript_projects/CloudChat/CloudChatClient /var/www/DOPS/CloudChat
+cp -r /var/lib/jenkins/workspace/GerritCloudChat serup branch/javascript_projects/DataEncoderDecoder /var/www/DOPS
+cp -r /var/lib/jenkins/workspace/GerritCloudChat serup branch/javascript_projects/socketserverAPI /var/www/DOPS
+cp -r /var/lib/jenkins/workspace/GerritCloudChat serup branch/codeblocks_projects/DataDictionary /var/www/DOPS
+#cd codeblocks_projects
 ./stopScanvaserver
 ./startScanvaserver
