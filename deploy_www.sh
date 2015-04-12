8b38ca34e74fbba0bfa8309c4ad94e46f0fafb8d
Johnny Serup <johnny.serup@scanva.com>
2015-04-12 15:03:41 +0200

    boble : still working on deploy script
    Change-Id: Id841f522053dc8ea6112f0443a76d5266b571246

--- deploy_www.sh
+++ deploy_www.sh
@@ -3,13 +3,14 @@ mkdir -p /var/www/DOPS/CloudChat/CloudChatManager
 mkdir -p /var/www/DOPS/CloudChat/CloudChatClient
 mkdir -p /var/www/DOPS/DataEncoderDecoder
 mkdir -p /var/www/DOPS/socketserverAPI
-#rm -rf /var/scanva/release/serup/
-#cd /var/scanva/release/
-#mkdir serup 
-#cd serup 
+rm -rf /var/scanva/release/serup/
+cd /var/scanva/release/
+mkdir serup 
+cd serup 
+git clone -b serup https://review.gerrithub.io/serup/CloudChat
 #git clone -b serup ssh://serup@review.gerrithub.io:29418/serup/CloudChat
-#cd CloudChat
-#./run.sh
+cd CloudChat
+./run.sh
 cp -r "/var/lib/jenkins/workspace/GerritCloudChat serup branch/javascript_projects/CloudChat/CloudChatManager" /var/www/DOPS/CloudChat
 cp -r "/var/lib/jenkins/workspace/GerritCloudChat serup branch/javascript_projects/CloudChat/CloudChatClient" /var/www/DOPS/CloudChat
 cp -r "/var/lib/jenkins/workspace/GerritCloudChat serup branch/javascript_projects/DataEncoderDecoder" /var/www/DOPS
