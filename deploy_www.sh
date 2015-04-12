a94c27ff73335f154378cb0fc4e7ec5da3065875
Johnny Serup <johnny.serup@scanva.com>
2015-04-12 13:16:10 +0200

    boble : deployment script updated to fit with serup branch - should deploy after success build
    Change-Id: I5c9aa7b2865edf4ce10b4f129da289004d54530e

--- deploy_www.sh
+++ deploy_www.sh
@@ -3,15 +3,18 @@ mkdir -p /var/www/DOPS/CloudChat/CloudChatManager
 mkdir -p /var/www/DOPS/CloudChat/CloudChatClient
 mkdir -p /var/www/DOPS/DataEncoderDecoder
 mkdir -p /var/www/DOPS/socketserverAPI
-rm -rf /var/scanva/release/master/
+rm -rf /var/scanva/release/serup/
 cd /var/scanva/release/
-mkdir master
-cd master
-git clone -b master https://github.com/serup/CloudChat.git
+mkdir serup 
+cd serup 
+git clone -b serup ssh://serup@review.gerrithub.io:29418/serup/CloudChat
 cd CloudChat
 ./run.sh
-cp -r /var/scanva/release/master/CloudChat/javascript_projects/CloudChat/CloudChatManager /var/www/DOPS/CloudChat
-cp -r /var/scanva/release/master/CloudChat/javascript_projects/CloudChat/CloudChatClient /var/www/DOPS/CloudChat
-cp -r /var/scanva/release/master/CloudChat/javascript_projects/DataEncoderDecoder /var/www/DOPS
-cp -r /var/scanva/release/master/CloudChat/javascript_projects/socketserverAPI /var/www/DOPS
-cp -r /var/scanva/release/master/CloudChat/codeblocks_projects/DataDictionary /var/www/DOPS
+cp -r /var/scanva/release/serup/CloudChat/javascript_projects/CloudChat/CloudChatManager /var/www/DOPS/CloudChat
+cp -r /var/scanva/release/serup/CloudChat/javascript_projects/CloudChat/CloudChatClient /var/www/DOPS/CloudChat
+cp -r /var/scanva/release/serup/CloudChat/javascript_projects/DataEncoderDecoder /var/www/DOPS
+cp -r /var/scanva/release/serup/CloudChat/javascript_projects/socketserverAPI /var/www/DOPS
+cp -r /var/scanva/release/serup/CloudChat/codeblocks_projects/DataDictionary /var/www/DOPS
+cd codeblocks_projects
+./stopScanvaserver
+./startScanvaserver
