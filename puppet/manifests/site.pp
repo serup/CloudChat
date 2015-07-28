370fd33fac6320e4a4d3b7f4bfe12173a80e32a5
serup <johnny.serup@scanva.com>
2015-07-28 10:14:05 +0200

    docker setup
    Change-Id: Ia57eb817fb0e61b6b49f734ff3cb1d68cd167c22

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,5 +1,5 @@
 node default {
-# Test message
+  # Test message
   notify { "Debug output on ${hostname} node.": }
   #include ntp, git
 }
@@ -8,31 +8,43 @@ node /^node01.*/ {
   # Test message
   notify { "Debug output on ${fqdn}": }
 
-  # Add adm group to sudoers with NOPASSWD
-  #sudo::conf { 'vagrant':
-  #  priority => 01,
-  #  content  => "vagrant ALL=(ALL) NOPASSWD: ALL",
-  #}
-
-  # Configuring apache
-#  include apache
-#  include apache::mod::php
-#
-#  apache::vhost { $::fqdn:
-#	  port => '80',
-#	       docroot => '/var/www/test',
-#	       require => File['/var/www/test'],
-#  }
-#
-#  # Setting up the document root
-#  file { ['/var/www', '/var/www/test'] : # [6]
-#	  ensure => directory,
-#  }
-#
-#  file { '/var/www/test/index.php' : # [7]
-#	  content => '>?php echo \'>p<Hello world!>/p<\' ?<',
-#  }
-
   class { apache : } 
-#  class { dops : }
+
+  # NB! Needed BEFORE docker class, otherwise it will fail during install
+  exec { "apt-update":
+    command => "/usr/bin/apt-get update"
+  }
+  Exec["apt-update"] -> Package <| |>
+
+#  class { apt : 
+#     notify  => Exec['apt_update']
+#  }
+  # By default, Puppet runs `apt-get update` on the first Puppet run after you include the `apt` class, and anytime `notify  => Exec['apt_update']` occurs; i.e., whenever config files get updated or other relevant changes occur. If you set `update['frequency']` to 'always', the update runs on every Puppet run. You can also set `update['frequency']` to 'daily' or 'weekly'
+
+  include 'docker'
+#  include sudo
+#
+#  # Add adm group to sudoers with NOPASSWD
+#  sudo::conf { 'vagrant':
+#    priority => 01,
+#    content  => "vagrant ALL=(ALL) NOPASSWD: ALL",
+#  }
+#
+##  class { dops : }
+#
+  docker::image { 'base':
+   ensure => 'absent',
+#   command => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
+  }
+
+  docker::image { 'ubuntu':
+   ensure  => 'absent',
+   tag     => 'precise'
+  }
+
+##  docker::run { 'helloworld':
+##        image   => 'ubuntu',
+## 	command => '/bin/sh -c "while true; do echo hello docker world; sleep 1; done"',
+##  }
+
 }
