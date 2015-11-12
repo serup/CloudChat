ad6960404f8d5a3799c468cb10e4f1ddc3a03dd2
Johnny Serup <serup@hotmail.com>
2015-11-12 10:06:55 +0100

    dependencies
    Change-Id: I5b9467b4e6f4efd9dae37dbea60d8a5650b3b2e8

--- bootstrap-node-manager.sh
+++ bootstrap-node-manager.sh
@@ -52,10 +52,10 @@ else
       echo "CloudChat installed"
     fi
     
-    echo "Create softlinks to g++-4.7"
-    sudo update-alternatives \
-    --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 \
-    --slave /usr/bin/g++ g++ /usr/bin/g++-4.7 
+#    echo "Create softlinks to g++-4.7"
+#    sudo update-alternatives \
+#    --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 \
+#    --slave /usr/bin/g++ g++ /usr/bin/g++-4.7 
 
 
 fi
