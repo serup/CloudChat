2b97ce541a4a36d82f93db39b2f60d268a220162
Johnny Serup <serup@hotmail.com>
2015-11-16 11:24:24 +0100

    Vagrant upgrade due to issue with hostname -- perhaps consider updating etc/hosts file on starting host also...
    Change-Id: I95819fd6ce99489c0410ea73501e36ef35a1949a

--- bootstrap-node-jenkins.sh
+++ bootstrap-node-jenkins.sh
@@ -73,7 +73,10 @@ else
       echo "CloudChat installed"
       echo "Set up swapfile"
       sudo bash addswapfile.sh
-      echo "Build CloudChat"
-      sudo ./run.sh
+      echo "Install Jenkins"
+      wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+      sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+      sudo apt-get update -yq
+      sudo apt-get install -yq jenkins
      fi
 fi
