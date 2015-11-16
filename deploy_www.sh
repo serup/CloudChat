3748f4c883adf331c140eaeec60385b49f2a4d7a
Johnny Serup <serup@hotmail.com>
2015-11-16 15:43:38 +0100

    deploying
    Change-Id: I8d743da051332ebd3c2e07174842f293ef55fd4d

--- deploy_www.sh
+++ deploy_www.sh
@@ -1,2 +1,5 @@
 sudo cp -a javascript_projects/CloudChat/CloudChatManager/. /var/www/
+sudo cp -r javascript_projects/DataEncoderDecoder/ /var/www/.
+sudo cp -r codeblocks_projects/DataDictionary/ /var/www/.
+sudo cp -r javascript_projects/socketserverAPI/ /var/www/.
 sudo cp /var/www/CloudChatManager.htm /var/www/index.html
