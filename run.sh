69d1c7d955856dc93071dfe79b236f004fb06e56
Johnny Serup <serup@hotmail.com>
2015-11-17 11:52:16 +0100

    total count in result test
    Change-Id: If126a1919098f825ac35caef120fd82eed3d8242

--- run.sh
+++ run.sh
@@ -2,3 +2,12 @@ cd codeblocks_projects
 make -f makefile_ubuntu.mak total 
 cd ../javascript_projects
 make -f makefile_ubuntu.mak total
+cd ..
+cat codeblocks_projects/total_result.txt > output.txt
+cat javascript_projects/total_result.txt >> output.txt
+(cat output.txt | grep 'Failed\|Passed\|---\|Test of\|OK\|FAIL' > result.txt; cat result.txt; (cat result.txt|grep -c 'OK';printf "")>pass.txt; (cat result.txt|grep -c 'FAIL';printf "")>fail.txt; echo Total; printf "Passed: ";cat pass.txt; printf "Failed: "; cat fail.txt ; echo ---------)>_total_result.txt
+rm fail.txt
+rm pass.txt
+rm result.txt
+cat _total_result.txt
+
