8041e67591690de8e01dba75d7a88ca23df1a30b
Johnny Serup <serup@hotmail.com>
2015-11-18 11:46:17 +0100

    only cloudchatmanager vm needs to deploy to www and NOT the database part, that is for backend.scanva.com
    Change-Id: I9f1cd35bd1eccdd69e0320a523d822f4cb6f682e

--- deploy_www.sh
+++ deploy_www.sh
@@ -1,5 +1,3 @@
 sudo cp -r javascript_projects/CloudChat/CloudChatManager/ /var/www/
 sudo cp -r javascript_projects/DataEncoderDecoder/ /var/www/.
-#sudo cp -r codeblocks_projects/DataDictionary/ /var/www/.
 sudo cp -r javascript_projects/socketserverAPI/ /var/www/.
-sudo cp /var/www/CloudChatManager/CloudChatManager.htm /var/www/index.html
