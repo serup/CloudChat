2e13b868cf72858d9fce0c0c646eefa6486e641f
Johnny Serup <serup@hotmail.com>
2015-11-25 15:49:11 +0100

    Add javaspring project
    Change-Id: Ica16265f85c7713205c5b70adff0ef15538f9b2d

--- bootstrap-node-javaservices.sh
+++ bootstrap-node-javaservices.sh
@@ -33,6 +33,53 @@ else
     sudo cp /vagrant/nodes.rc.local /etc/rc.local
     sudo puppet agent --enable
 
+    # Add agent section to /etc/puppet/puppet.conf
+    echo "" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null && \
+    echo "[agent]" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null && \
+    echo "    server=puppet" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null && \
+    echo "    environment=devtest" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null
+
+    sudo cp /vagrant/nodes.rc.local /etc/rc.local
+
+    # bugfix hostname for ubuntu 15.04 - run this command    
+    sudo bash /etc/init.d/hostname.sh
+
+
+    sudo puppet agent --enable
+
+    echo "fetch nodejs"
+    sudo apt-get install -yq nodejs-legacy
+
+    echo "fetch boost"
+    sudo apt-get install -yq  libboost-all-dev
+
+    echo "fetch xsltproc"
+    sudo apt-get install -yq xsltproc
+
+    echo "fetch g++ since somehow gcc default install does not get it"
+    sudo apt-get install -yq g++
+
+    echo "Fetch latest version of CloudChat"
+    if [ -d "CloudChat" ]; then
+      echo "CloudChat already installed"
+      echo "updating ..."
+      cd CloudChat
+      git checkout serup
+      git pull
+    else
+      git clone https://review.gerrithub.io/serup/CloudChat
+      cd CloudChat
+      git checkout serup
+      echo "CloudChat installed"
+      echo "Set up swapfile"
+      sudo bash addswapfile.sh
+      #echo "Build CloudChat"
+      #sudo ./run.sh
+      #echo "Deploy to www"
+      #sudo bash deploy_www.sh
+    fi
+
+
     echo "echo \"---------\" " >> .bashrc
     echo "echo \"\" " >> .bashrc
     echo "sudo grails --version" >> .bashrc
