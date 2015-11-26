4f46c2593a2c4ce7336b0f929cc3157aeb28d67c
Johnny Serup <serup@hotmail.com>
2015-11-26 12:16:54 +0100

    BugFix - revert
    Change-Id: I20c7de8f2290d3407fbb0191ce6ce4c7a9f6ecf0

--- confighosts.sh
+++ confighosts.sh
@@ -1,10 +1,10 @@
     # Configure /etc/hosts file
     echo "" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "# Host config for Puppet Master and Agent Nodes" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.8    puppet.dops.local           puppet" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.19   javaservices.scanva.com     javaservices" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.21   backend.scanva.com          backend" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.22   cloudchatmanager.com        cloudchatmanager" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.23   cloudchatclient.com         cloudchatclient" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.32.20   jenkins.scanva.com          jenkins" | sudo tee --append /etc/hosts 2> /dev/null
+    echo "192.168.31.8    dops.puppet.master           puppet" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.31.19   javaservices.scanva.com     javaservices" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.31.21   backend.scanva.com          backend" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.31.22   cloudchatmanager.com        cloudchatmanager" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.31.23   cloudchatclient.com         cloudchatclient" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.31.20   jenkins.scanva.com          jenkins" | sudo tee --append /etc/hosts 2> /dev/null
 
