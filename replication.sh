d8f4f3d735a95ab9b0524ce9211946954f156bae
Johnny Serup <serup@hotmail.com>
2015-11-30 14:41:56 +0100

    boble - work on image replication
    Change-Id: If1202cc1cabd79bba43c4daa50cb49e6c4507993

--- replication.sh
+++ replication.sh
@@ -1,2 +1,3 @@
 #!/bin/bash 
-sshpass -p "vagrant" scp -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/CloudChatManager/.
+sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/CloudChatManager/.
+rm -f /var/www/img/*
