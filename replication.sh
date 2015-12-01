ad698f436118c708e88eef89ba60333a58f60cc6
Johnny Serup <serup@hotmail.com>
2015-12-01 13:31:49 +0100

    issue with remove of tmp image
    Change-Id: I41693e6c6e8044f02be4dcf404ef7592dec1c395

--- replication.sh
+++ replication.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
 sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/CloudChatManager/.
-rm -f /var/www/img/*
+#sleep 1000
+#rm -f /var/www/img/*
