b58225449f682f42412996499704b573799664d3
Johnny Serup <serup@hotmail.com>
2015-12-03 13:15:47 +0100

    NB! broken build, however new rmyoung.sh script - somehow coverage is still causing issues
    Change-Id: I19df3da1f73c94ec1f8d95e0b7cf18e3aa2bf1e3

---
+++ rmyoung.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# should remove files of older iteration
+# example:
+# <filename>_<iterationnumber>.jpg
+# ABC_8.jpg
+# ABC_7.jpg - remove
+# ABC_6.jpg - remove
+#
+# usage : rmyoung.sh ABC_??.jpg 
+#
+# 
+filename=$1
+prefix="${filename%%??.*}"
+echo "prefix: $prefix"
+suffix="${filename##*.}"
+echo "suffix: $suffix"
+echo found files :
+ls -1 ${prefix}??.*
+echo now remove all except last filename
+lastfile=$(ls -1 ${prefix}??.* | sort -V | tail -1)
+echo "lastfile = ${lastfile}"
+echo remove following :
+ls -1 ${prefix}??.* | grep -v ${lastfile}
