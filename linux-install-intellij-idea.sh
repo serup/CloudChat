0b464b56877803bc2b3dc1ead5cea05958aef73f
Johnny serup <serup@hotmail.com>
2015-12-15 21:04:24 +0100

    add install script for intellij to use outside puppet
    Change-Id: Ie00ae7148b496fb3b25689afaa453c6c45396dd6

---
+++ linux-install-intellij-idea.sh
@@ -0,0 +1,35 @@
+# Specify IDEA version here.
+IDEA_PACKAGE=ideaIU-15.0.2.tar.gz
+
+# Assuming we already did this during the previous steps:
+# sudo add-apt-repository ppa:webupd8team/java
+# sudo apt-get update
+# sudo apt-get install oracle-java7-installer oracle-java8-installer
+
+wget -O "/tmp/$IDEA_PACKAGE" http://download-cf.jetbrains.com/idea/$IDEA_PACKAGE
+cd /tmp
+
+if [ -f "/tmp/$IDEA_PACKAGE" ]; then
+  tar -xzvf "$IDEA_PACKAGE"
+
+  IDEA_EXTRACTED_FOLDER=`ls -dt idea-IU* | head -1`
+  sudo rsync -av "$IDEA_EXTRACTED_FOLDER/" /opt/idea/
+  sudo cp /opt/idea/bin/idea.png /usr/share/pixmaps/
+
+  echo '[Desktop Entry]
+Name=IntelliJ IDEA
+Type=Application
+Exec=idea.sh
+Terminal=false
+Icon=idea
+Comment=Integrated Development Environment
+NoDisplay=false
+Categories=Development;IDE;
+Name[en]=IntelliJ IDEA
+' > idea.desktop
+
+  sudo desktop-file-install idea.desktop
+  sudo rm -f idea.desktop
+  sudo rm -f /usr/local/bin/idea
+  sudo ln -s /opt/idea/bin/idea.sh /usr/local/bin/idea
+fi
