9ce6875082e7618d888fc2b5a6661b8ca4bce26a
Johnny Serup <serup@hotmail.com>
2015-12-23 13:54:50 +0100

    SUCCESS encode - websocket transfer - decode
    Change-Id: I5e4249652efa654d334c44e2f49b9232921e5746

--- javaspring_projects/javaservice-webapp/complete/src/main/java/ClientEndpoint/JavaWebSocketClientEndpoint.java
+++ javaspring_projects/javaservice-webapp/complete/src/main/java/ClientEndpoint/JavaWebSocketClientEndpoint.java
@@ -1,12 +1,18 @@
 package ClientEndpoint;
 
 import ClientMessageHandler.JavaWebSocketClientMessageHandler;
+import org.glassfish.tyrus.client.ClientManager;
 
+import javax.websocket.ClientEndpointConfig;
 import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
 import javax.websocket.Session;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by serup on 23-12-15.
@@ -15,10 +21,15 @@ public final class JavaWebSocketClientEndpoint extends Endpoint
 {
     public Session clientSession;
     JavaWebSocketClientMessageHandler msgHandler;
+    final ClientEndpointConfig cec;
+    ClientManager client;
 
     public JavaWebSocketClientEndpoint()
     {
+        cec = ClientEndpointConfig.Builder.create().build();
+        client = ClientManager.createClient();
         msgHandler = new JavaWebSocketClientMessageHandler();
+        msgHandler.messageLatch = new CountDownLatch(1);
     }
 
     @Override
@@ -31,6 +42,16 @@ public final class JavaWebSocketClientEndpoint extends Endpoint
         }
     }
 
+    public void connectToServer(String strURI)
+    {
+        try {
+            client.connectToServer(this, cec, new URI(strURI));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
     public int sendToServer(ByteBuffer data)
     {
         int result=-1;
@@ -46,5 +67,17 @@ public final class JavaWebSocketClientEndpoint extends Endpoint
             }
         }
         return result;
+    }
+
+    public byte[] receiveFromServer()
+    {
+        byte[] data = null;
+        try{
+            msgHandler.messageLatch.await(100, TimeUnit.SECONDS); // wait for incomming data
+            data = msgHandler.receivedData;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
     }
 }
--- javaspring_projects/javaservice-webapp/complete/src/main/test/WebSocketTests.java
+++ javaspring_projects/javaservice-webapp/complete/src/main/test/WebSocketTests.java
@@ -1,6 +1,7 @@
 import WebSocketEchoTestEndpoints.EchoByteArrayEndpoint;
 import ClientEndpoint.JavaWebSocketClientEndpoint;
 import WebSocketEchoTestEndpoints.EchoEndpoint;
+import messaging.simp.ded.DEDDecoder;
 import messaging.simp.ded.DEDEncoder;
 import org.glassfish.tyrus.server.Server;
 import org.glassfish.tyrus.client.ClientManager;
@@ -133,14 +134,12 @@ public class WebSocketTests {
         /**
          * setup client
          */
-        final ClientEndpointConfig cec = ClientEndpointConfig.Builder.create().build();
-        ClientManager client = ClientManager.createClient();
         JavaWebSocketClientEndpoint clientEndpoint = new JavaWebSocketClientEndpoint();
 
         /**
          * connect client
          */
-        client.connectToServer(clientEndpoint,cec, new URI("ws://localhost:8033/websockets/echoBinary"));
+        clientEndpoint.connectToServer("ws://localhost:8033/websockets/echoBinary");
 
         /**
          * prepare data to be send
@@ -164,8 +163,36 @@ public class WebSocketTests {
         clientEndpoint.sendToServer(data);
 
         /**
-         * wait for response from server
+         * wait for incomming data response, then receive data
          */
+        byte[] receivedData = clientEndpoint.receiveFromServer();
+
+        /**
+         * decode incomming data
+         */
+        boolean bDecoded=false;
+        String strValue="";
+        short iValue=0;
+        boolean bValue=false;
+        String strText="";
+
+        // decode data ...
+        DEDDecoder DED2 = new DEDDecoder();
+        DED2.PUT_DATA_IN_DECODER( receivedData, receivedData.length);
+        if( DED2.GET_STRUCT_START( "event" )==1 &&
+                (strValue = DED2.GET_METHOD ( "name" )).length()>0 &&
+                (iValue   = DED2.GET_USHORT ( "trans_id")) !=-1 &&
+                (bValue   = DED2.GET_BOOL   ( "startstop")) &&
+                (strText  = DED2.GET_STDSTRING ( "text")).length()>0 &&
+                DED2.GET_STRUCT_END( "event" )==1)
+        {
+            bDecoded=true;
+        }
+        else
+        {
+            bDecoded=false;
+        }
+
 
     }
 
