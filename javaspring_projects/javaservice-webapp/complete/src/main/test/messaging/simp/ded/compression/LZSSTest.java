8b163c444187eaa7e58a6ae054f53bc6168601bf
Johnny Serup <serup@hotmail.com>
2015-12-28 13:48:33 +0100

    merge from serup gerrit

---
+++ javaspring_projects/javaservice-webapp/complete/src/main/test/messaging/simp/ded/compression/LZSSTest.java
@@ -0,0 +1,21 @@
+package messaging.simp.ded.compression;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by serup on 28-12-15.
+ */
+public class LZSSTest {
+
+    @Test
+    public void testCompress() throws Exception {
+
+    }
+
+    @Test
+    public void testUncompress() throws Exception {
+
+    }
+}
