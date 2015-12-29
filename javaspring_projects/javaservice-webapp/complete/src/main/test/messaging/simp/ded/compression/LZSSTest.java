ff59d65f940a8f9e98fbe38bb36638ca5b3b70ef
Johnny Serup <serup@hotmail.com>
2015-12-29 13:34:18 +0100

    add resource file for testing compression decompression
    Change-Id: I9e6fb323d07d1624c8ac0f354b343370ad91b8d7

--- javaspring_projects/javaservice-webapp/complete/src/main/test/messaging/simp/ded/compression/LZSSTest.java
+++ javaspring_projects/javaservice-webapp/complete/src/main/test/messaging/simp/ded/compression/LZSSTest.java
@@ -1,13 +1,21 @@
 package messaging.simp.ded.compression;
+
 import messaging.simp.ded.DEDDecoder;
 import messaging.simp.ded.DEDEncoder;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.MissingResourceException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.fail;
 
 /**
  * Created by serup on 28-12-15.
@@ -17,19 +25,47 @@ public class LZSSTest {
     DEDEncoder dedEncoder = new DEDEncoder();
     DEDDecoder dedDecoder = new DEDDecoder();
 
+    public static byte[] getBytes(InputStream is) throws IOException {
+
+        int len;
+        int size = 1024;
+        byte[] buf;
+
+        if (is instanceof ByteArrayInputStream) {
+            size = is.available();
+            buf = new byte[size];
+            len = is.read(buf, 0, size);
+        } else {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            buf = new byte[size];
+            while ((len = is.read(buf, 0, size)) != -1)
+                bos.write(buf, 0, len);
+            buf = bos.toByteArray();
+        }
+        return buf;
+    }
+
+
     @Test
     public void testCompression() {
         InputStream txtfile = null;
-/*        try {
-            txtfile =  getClass().getResourceAsStream("helloworld.txt");
+        byte[] originalTxtfileInBytes=null;
+        try {
+            Resource resource = null;
+            resource = new FileSystemResource("src/main/resources/helloworld.txt");
+            InputStream file = (InputStream) resource.getInputStream();
+            originalTxtfileInBytes = getBytes(file);
         } catch (MissingResourceException e) {
             fail();
         } catch (Exception e) {
             fail();
         }
-        */
-        String originaltxt = "ATATAAAFFFF";
-        txtfile = new ByteArrayInputStream(originaltxt.getBytes());
+
+
+        //String originaltxt = "ATATAAAFFFF";
+        //txtfile = new ByteArrayInputStream(originaltxt.getBytes());
+
+        txtfile = new ByteArrayInputStream(originalTxtfileInBytes);
 
         Compressor compressor = new LZSS(txtfile);
         ByteArrayOutputStream bosCompressed = null;
@@ -50,8 +86,10 @@ public class LZSSTest {
         String result;
         try {
             byte[] back = bosUncompressed.toByteArray();
-            result = new String(back, "UTF-8");
-            assertEquals(originaltxt,result);
+         //   result = new String(back, "UTF-8");
+         //   assertEquals(originaltxt,result);
+
+            assertArrayEquals(originalTxtfileInBytes,back);
         } catch (Exception e) {
             fail();
             return;
