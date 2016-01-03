dd7ff440e3a371c822660a9fccd7e30b3b82dce9
Johnny Serup <johnny.serup@scanva.com>
2016-01-03 11:46:42 +0100

    boble - add more to integration test - NB! webserver MUST be runnning before test can run
    Change-Id: If3c68db0e1f58a3e6d0d49bc903f3aab9e54553f

--- javaspring_projects/javaservice-webapp/complete/src/main/test/integrationTests/DOPSconnectTests.java
+++ javaspring_projects/javaservice-webapp/complete/src/main/test/integrationTests/DOPSconnectTests.java
@@ -1,9 +1,32 @@
 package integrationTests;
 
+import ClientEndpoint.JavaWebSocketClientEndpoint;
+import org.junit.Test;
+
 /**
  * Created by serup on 31-12-15.
  */
 public class DOPSconnectTests {
+
+    @Test
+    public void testConnectToDFD()
+    {
+        /**
+        * setup client
+        */
+        JavaWebSocketClientEndpoint clientEndpoint = new JavaWebSocketClientEndpoint();
+
+        /**
+         * connect client
+         */
+        clientEndpoint.connectToServer("ws://localhost:8033");
+
+        /**
+         * prepare data to be send
+         */
+
+
+    }
 
 
 }
