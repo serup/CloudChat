f7e79929621be72bfecaada5ce23b5ca352db6af
Johnny Serup <serup@hotmail.com>
2016-01-05 12:12:15 +0100

    Try - BugFix defunct processes
    Change-Id: I46c16f87b673cf1a6439119ee06882f282a558f3

--- replication.sh
+++ replication.sh
@@ -2,8 +2,8 @@
 # First resize to a maximum of 200x200 pixels
 mogrify -resize 200x200 *.jpg
 # Then transfer files to frontend server
-sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/CloudChatManager/.
-sleep 1000
+sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/CloudChatManager/. 
+#sleep 1000
 # Cleanup
 rm -f /var/www/img/*
 exec "$@"
