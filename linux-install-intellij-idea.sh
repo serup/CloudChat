349444848ed83a513d6e9c5b8588a38f0c42e908
serup <serup@hotmail.com>
2016-01-08 08:56:55 +0100

    bugfix - avoid trailing slash resulting in recursive folder create
    Change-Id: I59a9dfdc8cc3dbf020e45a23d7a402663c428cc8

--- linux-install-intellij-idea.sh
+++ linux-install-intellij-idea.sh
@@ -2,18 +2,26 @@
 IDEA_PACKAGE=ideaIC-15.0.2.tar.gz
 
 # Assuming we already did this during the previous steps:
-# sudo add-apt-repository ppa:webupd8team/java
-# sudo apt-get update
-# sudo apt-get install oracle-java7-installer oracle-java8-installer
-
-wget -O "/tmp/$IDEA_PACKAGE" http://download-cf.jetbrains.com/idea/$IDEA_PACKAGE
-cd /tmp
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' 2>&1 oracle-java8-installer |grep "install ok installed")
+if ["" == "$PKG_OK" ]; then
+# install essentials
+        sudo add-apt-repository ppa:webupd8team/java
+        sudo apt-get update
+        sudy apt-get install oracle-java7-installer oracle-java8-installer
+fi
 
 if [ -f "/tmp/$IDEA_PACKAGE" ]; then
+  cd /tmp
   tar -xzvf "$IDEA_PACKAGE"
+else
+  wget -O "/tmp/$IDEA_PACKAGE" http://download-cf.jetbrains.com/idea/$IDEA_PACKAGE
+  cd /tmp
+  if [ -f "/tmp/$IDEA_PACKAGE" ]; then
+ 	tar -xzvf "$IDEA_PACKAGE"
+fi
 
   IDEA_EXTRACTED_FOLDER=`ls -dt idea-IC* | head -1`
-  sudo rsync -av "$IDEA_EXTRACTED_FOLDER/" /opt/idea/
+  sudo rsync -av "$IDEA_EXTRACTED_FOLDER/" /opt/idea
   sudo cp /opt/idea/bin/idea.png /usr/share/pixmaps/
 
   echo '[Desktop Entry]
