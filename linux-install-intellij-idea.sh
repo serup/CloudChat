a179c28a37e78fd8d97e199f5e7e304950f9d72d
serup <serup@hotmail.com>
2016-01-08 09:55:36 +0100

    bugfix - install reinstall intellij
    Change-Id: If7c79d9a68c5684152214dec8db77c95010efca5

--- linux-install-intellij-idea.sh
+++ linux-install-intellij-idea.sh
@@ -10,6 +10,10 @@ if ["" == "$PKG_OK" ]; then
         sudy apt-get install oracle-java7-installer oracle-java8-installer
 fi
 
+if [ -d "/opt/idea" ]; then
+   echo "/opt/idea ALREADY exists - please remove first before running this again, and if you want a reinstall, then remove hidden file in home directory .ideaIC15 and then delete installation directory for intellij"
+else
+
 if [ -f "/tmp/$IDEA_PACKAGE" ]; then
   cd /tmp
   tar -xzvf "$IDEA_PACKAGE"
@@ -21,7 +25,7 @@ else
 fi
 
   IDEA_EXTRACTED_FOLDER=`ls -dt idea-IC* | head -1`
-  sudo rsync -av "$IDEA_EXTRACTED_FOLDER/" /opt/idea
+  sudo rsync -av "$IDEA_EXTRACTED_FOLDER/" /opt/idea/
   sudo cp /opt/idea/bin/idea.png /usr/share/pixmaps/
 
   echo '[Desktop Entry]
@@ -40,4 +44,6 @@ Name[en]=IntelliJ IDEA
   sudo rm -f idea.desktop
   sudo rm -f /usr/local/bin/idea
   sudo ln -s /opt/idea/bin/idea.sh /usr/local/bin/idea
+fi
+
 fi
