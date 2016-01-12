9a91153bfc0a4f05b82a00217ab72ab167365ab9
serup <serup@hotmail.com>
2016-01-12 14:32:14 +0100

    CLEANup puppet - plus add init for new node hadoop
    Change-Id: I2d131a7ee86046a70b040d7d4a727c0ebbceaf43

--- confighosts.sh
+++ confighosts.sh
@@ -2,6 +2,7 @@
     echo "" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "# Host config for Puppet Master and Agent Nodes" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "192.168.31.8    dops.puppet.master           puppet" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.31.18   hadoop.scanva.com           hadoop" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "192.168.31.19   javaservices.scanva.com     javaservices" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "192.168.31.21   backend.scanva.com          backend" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "192.168.31.22   cloudchatmanager.com        cloudchatmanager" | sudo tee --append /etc/hosts 2> /dev/null && \
