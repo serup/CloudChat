878241ab917ae3b3b7c601a4e2ff6c1e938e7785
serup <serup@hotmail.com>
2016-01-12 09:09:11 +0100

    add taskwarrior and more on mockserver handling of CreateProfile request
    Change-Id: I8d7d536504dd430f6f0aea6546de9c349cf14bba

--- install.sh
+++ install.sh
@@ -37,6 +37,14 @@ if [ "docker-image-skeleton" == "$image" ]; then
 else
    echo "- docker images NOT installed - please check setup.conf to see if it is correct"
 fi
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' 2>&1 taskwarrior* |grep "install ok installed")
+if [ "" == "$PKG_OK" ]; then
+  echo -n "- install taskwarrior "
+  sudo apt-get install -yq taskwarrior
+  echo " - done."
+else
+  echo "- taskwarrior already installed"
+fi
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' 2>&1 xslt* |grep "install ok installed")
 if [ "" == "$PKG_OK" ]; then
   echo -n "- install xsltproc "
