6a570cd51e29170e5bf1d42adeadaa6e5263a856
serup <serup@hotmail.com>
2016-01-12 14:32:14 +0100

    CLEANup puppet - plus add init for new node hadoop
    Change-Id: I2d131a7ee86046a70b040d7d4a727c0ebbceaf43

--- puppet/trunk/environments/devtest/modules/dops/manifests/params.pp
+++
@@ -1,5 +0,0 @@
-class dops::params {
-#  $packages = ['libxslt-dev', 'libxml2-dev', 'default-jdk', 'ruby', 'ruby-dev', 'zlib1g-dev', 'zlibc']
-#  $confluence_server     = "confluence.dops.scanva.com"
-}
-~
