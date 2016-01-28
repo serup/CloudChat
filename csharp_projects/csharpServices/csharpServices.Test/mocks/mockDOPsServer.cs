e624bfb670efc8c61d9156bee9c11deaef07d9ba
serup <serup@hotmail.com>
2016-01-28 09:57:12 +0100

    add handler for DOPs DED packets in mockDOPsServer
    Change-Id: I5a4c27cca1143ea87fec05c4a6bf1d38e44cafe6

--- csharp_projects/csharpServices/csharpServices.Test/mocks/mockDOPsServer.cs
+++ csharp_projects/csharpServices/csharpServices.Test/mocks/mockDOPsServer.cs
@@ -9,27 +9,22 @@ using DED;
 
 class handleDOPsProtocol
 {
+	// Decoding of DED packets with regards to mock DOPs Server
 	public byte[] ReceiveDEDandResponse(byte[] dedpacket)
 	{
-		//TODO: add decoding of DED packets with regards to mock DOPs Server
-
-		/*
-		 * java snippet - please convert 
-		 */
-		/* - converting in progress
 		bool bDecoded = false;
-		string strMethod, strProtocolTypeID, strUsername, strPassword, strFunctionName, strStatus;
-		short uTrans_id;
+		string strMethod="", strProtocolTypeID="", strUsername="", strPassword="", strFunctionName="", strStatus="";
+		short uTrans_id=0;
 		// 1. decode login packet
 		DEDDecoder DED = new DEDDecoder();
 		DED.PUT_DATA_IN_DECODER( dedpacket, dedpacket.Length);
 		if( DED.GET_STRUCT_START( "WSRequest" )==1 &&
-			(strMethod = DED.GET_METHOD ( "name" )).Length>0 &&
-			(uTrans_id = DED.GET_USHORT ( "trans_id")) !=-1 &&
-			(DED.GET_STDSTRING ( "protocolTypeID")).Length>0 &&
-			(DED.GET_STDSTRING ( "functionName")).Length>0 &&
-			(DED.GET_STDSTRING ( "username")).Length>0 &&
-			(DED.GET_STDSTRING ( "password")).Length>0 &&
+				(strMethod = DED.GET_METHOD ( "name" )).Length>0 &&
+				(uTrans_id = DED.GET_USHORT ( "trans_id")) !=-1 &&
+				(DED.GET_STDSTRING ( "protocolTypeID")).Length>0 &&
+				(DED.GET_STDSTRING ( "functionName")).Length>0 &&
+				(DED.GET_STDSTRING ( "username")).Length>0 &&
+				(DED.GET_STDSTRING ( "password")).Length>0 &&
 			DED.GET_STRUCT_END( "WSRequest" )==1)
 		{
 			bDecoded=true;
@@ -40,7 +35,7 @@ class handleDOPsProtocol
 			String username = "johndoe@email.com";
 			String password = "12345";
 
-			if(!strMethod.Equals("JavaConnect")) bDecoded=false;
+			if(!strMethod.Equals("CSharpConnect")) bDecoded=false;
 			if(uTrans_id != trans_id) bDecoded=false;
 			if(!strFunctionName.Equals(uniqueId)) bDecoded=false;
 			if(!strProtocolTypeID.Equals("DED1.00.00")) bDecoded=false;
@@ -63,21 +58,21 @@ class handleDOPsProtocol
 				{
 					Console.WriteLine("- EmployeeRequest - received - now parse");
 					String strProfileID = "", strProfileName = "", strSizeOfProfileData = "", strProfile_chunk_id = "", strAccountStatus = "", strExpireDate = "", strProfileStatus = "";
-					if((strProfileID = DED.GET_STDSTRING("profileID")).Length>0 &&
-						(strProfileName = DED.GET_STDSTRING("profileName")).Length>0 &&
-						(strProtocolTypeID = DED.GET_STDSTRING("protocolTypeID")).Length>0 &&
-						(strSizeOfProfileData = DED.GET_STDSTRING("sizeofProfileData")).Length>0 &&
-						(strProfile_chunk_id = DED.GET_STDSTRING("profile_chunk_id")).Length>0 &&
-						(strAccountStatus = DED.GET_STDSTRING("AccountStatus")).Length>0 &&
-						(strExpireDate = DED.GET_STDSTRING("ExpireDate")).Length>0 &&
-						(strProfileStatus = DED.GET_STDSTRING("ProfileStatus")).Length>0)
+					if((strProfileID 			= DED.GET_STDSTRING("profileID")).Length>0 &&
+						(strProfileName 		= DED.GET_STDSTRING("profileName")).Length>0 &&
+						(strProtocolTypeID 		= DED.GET_STDSTRING("protocolTypeID")).Length>0 &&
+						(strSizeOfProfileData 	= DED.GET_STDSTRING("sizeofProfileData")).Length>0 &&
+						(strProfile_chunk_id 	= DED.GET_STDSTRING("profile_chunk_id")).Length>0 &&
+						(strAccountStatus 		= DED.GET_STDSTRING("AccountStatus")).Length>0 &&
+						(strExpireDate 			= DED.GET_STDSTRING("ExpireDate")).Length>0 &&
+						(strProfileStatus 		= DED.GET_STDSTRING("ProfileStatus")).Length>0)
 					{
 						Console.WriteLine("-- Employee record received - now validate TOAST ");
 						if((DED.GET_STDSTRING("STARTtoast")).Length>0 )
 						{
 							// TOAST area found, now iterate thru all elements
 							Console.WriteLine("--- TOAST area found, now iterate thru all elements");
-							DEDDecoder._Elements elementvalue = null;
+							DED._Elements elementvalue = null;
 							while((elementvalue = DED.GET_ELEMENT("profile"))!=null)
 							{
 								Console.WriteLine("--- TOAST element : " + elementvalue.strElementID);
@@ -103,12 +98,12 @@ class handleDOPsProtocol
 							// 3. create response packet
 							DEDEncoder DED2 = new DEDEncoder();
 							DED2.PUT_STRUCT_START( "DFDResponse" );
-							DED2.PUT_METHOD   ( "Method", "CreateProfile" );
-							DED2.PUT_USHORT   ( "TransID", uTrans_id);
-							DED2.PUT_STDSTRING( "protocolTypeID", "DED1.00.00");
-							DED2.PUT_STDSTRING( "dest", strClientSrc );
-							DED2.PUT_STDSTRING( "src", "DFD_1.1" );
-							DED2.PUT_STDSTRING( "status", strStatus );
+								DED2.PUT_METHOD   ( "Method", "CreateProfile" );
+								DED2.PUT_USHORT   ( "TransID", uTrans_id);
+								DED2.PUT_STDSTRING( "protocolTypeID", "DED1.00.00");
+								DED2.PUT_STDSTRING( "dest", strClientSrc );
+								DED2.PUT_STDSTRING( "src", "DFD_1.1" );
+								DED2.PUT_STDSTRING( "status", strStatus );
 							DED2.PUT_STRUCT_END( "DFDResponse" );
 
 							byte[] _dedResponsePacket = DED2.GET_ENCODED_BYTEARRAY_DATA();
@@ -146,11 +141,11 @@ class handleDOPsProtocol
 		// 3. create response packet
 		DEDEncoder DED3 = new DEDEncoder();
 		DED3.PUT_STRUCT_START( "WSResponse" );
-		DED3.PUT_METHOD   ( "name", strMethod );
-		DED3.PUT_USHORT   ( "trans_id", uTrans_id);
-		DED3.PUT_STDSTRING( "protocolTypeID", "DED1.00.00");
-		DED3.PUT_STDSTRING( "functionName", strFunctionName );
-		DED3.PUT_STDSTRING( "status", strStatus );
+			DED3.PUT_METHOD   ( "name", strMethod );
+			DED3.PUT_USHORT   ( "trans_id", uTrans_id);
+			DED3.PUT_STDSTRING( "protocolTypeID", "DED1.00.00");
+			DED3.PUT_STDSTRING( "functionName", strFunctionName );
+			DED3.PUT_STDSTRING( "status", strStatus );
 		DED3.PUT_STRUCT_END( "WSResponse" );
 
 		byte[] dedResponsePacket = DED3.GET_ENCODED_BYTEARRAY_DATA();
@@ -161,9 +156,6 @@ class handleDOPsProtocol
 			Console.WriteLine("Internal ERROR [MockServer] - was not capable of creating a DED response packet, thus echoing received back");
 		}
 		return dedResponsePacket;
-		*/
-
-		return null;
 	}
 
 }
@@ -192,8 +184,6 @@ class MockDOPsServer {
 		while (!bDone) {
 			Console.WriteLine ("busy..");
 			c++;
-			// finished with work
-			//if (c > 100000) {
 			if (c > additioncycles) {
 				StopDOPsServer (); bDone=true;
 			}
@@ -208,9 +198,7 @@ class MockDOPsServer {
 		public void DoWork()
 		{
 			allSockets = new System.Collections.Generic.List<IWebSocketConnection>();
-//			server = new WebSocketServer("ws://127.0.0.1:8046/websockets/MockServerEndpoint"); // NB! running as mockserver in java spring project
 			server = new WebSocketServer("ws://127.0.0.1:" + _port.ToString() + _endpoint.ToString()); // NB! running as mockserver in java spring project
-
 			server.Start(socket =>
 				{
 					socket.OnOpen = () =>
@@ -237,31 +225,13 @@ class MockDOPsServer {
 					{
 						Console.WriteLine("mockDOPsServer Received blob from WebSocketClient");
 						try {
-						short trans_id = 1;
-						bool bAction = true;
-						bool bDecoded = false;
-						DEDDecoder DED2 = DEDDecoder.DED_START_DECODER();
-						DED2.PUT_DATA_IN_DECODER (blob, blob.Length);
-						if ((DED2.GET_STRUCT_START ("event")).Equals(1) &&
-							(	DED2.GET_METHOD ("Method")).Contains("MusicPlayer") &&
-							(	DED2.GET_USHORT ("trans_id")).Equals(trans_id) &&
-							(	DED2.GET_BOOL   ("startstop")).Equals(bAction) &&
-							(DED2.GET_STRUCT_END("event")).Equals(1)) 
-						{
-							bDecoded = true;
-							Console.WriteLine("mockDOPsServer Received DED and decoded it from WebSocketClient");
 
-							// Send it back as echo
+							handleDOPsProtocol hDOPs = new handleDOPsProtocol();
+							byte[] dedpacket = hDOPs.ReceiveDEDandResponse(blob);
+								
+							// Send it back as echo - until unittest have changed to handle response DED from above ReceiveDEDandResponse function
 							socket.Send(blob).Wait(CancellationToken.None);
-							Thread.Sleep(1000);
-						}
-						else {
-							bDecoded = false;
-							// Send it back as echo
-							socket.Send(blob).Wait(CancellationToken.None);
-							Thread.Sleep(1000);
-						}
-
+							//Thread.Sleep(1000);
 						}
 						catch(Exception e){
 							Console.WriteLine("WARNING: Exception while trying to decode possible DED blob");
