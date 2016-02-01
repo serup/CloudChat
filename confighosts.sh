169454066b2d062a216a109fdbf746a8e226bd57
serup <serup@hotmail.com>
2016-02-01 13:32:22 +0100

    bugfix - hostname must be one.cluster etc.. otherwise ambari dashboard does not work
    Change-Id: I3e494cd671c8904fcde2c669b842ad488e5e9599

--- confighosts.sh
+++ confighosts.sh
@@ -2,10 +2,10 @@
     echo "" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "# Host config for Puppet Master and Agent Nodes" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "192.168.31.8    dops.puppet.master               puppet" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.31.15   jekyll.dops.scanva.com           hadoop1" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.0.101   hadoop1.dops.scanva.com          hadoop1" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.0.102   hadoop2.dops.scanva.com          hadoop2" | sudo tee --append /etc/hosts 2> /dev/null && \
-    echo "192.168.0.103   hadoop3.dops.scanva.com          hadoop3" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.31.15   jekyll.dops.scanva.com           jekyll" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.0.101   one.cluster                      hadoop1" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.0.102   two.cluster                      hadoop2" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.0.103   three.cluster                    hadoop3" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "192.168.31.19   javaservices.dops.scanva.com     javaservices" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "192.168.31.21   backend.dops.scanva.com          backend" | sudo tee --append /etc/hosts 2> /dev/null && \
     echo "192.168.31.22   cloudchatmanager.com             cloudchatmanager" | sudo tee --append /etc/hosts 2> /dev/null && \
