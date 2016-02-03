d8924bb73647ed0f5e31cc337a967093e246751d
serup <serup@hotmail.com>
2016-02-03 15:11:58 +0100

    boble
    Change-Id: I290450c9d8c22b295843b5b620b0e5de8e97da8a

--- python_projects/ded/test/test_DED.py
+++ python_projects/ded/test/test_DED.py
@@ -1,33 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-from array import array
 from ded import ded
-
-import six
+import unittest
 import sys
 sys.path[0:0] = [""]
-
-import os
-import os.path
-import base64
-try:
-    from ssl import SSLError
-except ImportError:
-    # dummy class of SSLError for ssl none-support environment.
-    class SSLError(Exception):
-        pass
-
-if sys.version_info[0] == 2 and sys.version_info[1] < 7:
-    import unittest2 as unittest
-else:
-    import unittest
-
-import uuid
-
-if six.PY3:
-    from base64 import decodebytes as base64decode
-else:
-    from base64 import decodestring as base64decode
 
 
 class DEDTest(unittest.TestCase):
@@ -48,3 +24,4 @@ class DEDTest(unittest.TestCase):
         self.assertTrue(result == 1, result)
         result = DED.PUT_STRUCT_END(DED, "event")
         self.assertTrue(result > 0, result)
+
