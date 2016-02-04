d6ba57ab0ef2b795ba35a8db687463e8234a0edf
serup <serup@hotmail.com>
2016-02-04 14:35:48 +0100

    add testDED_PUT_BOOL
    Change-Id: I06cd89f27fd3388cbec83b9030a7fd1292036926

--- python_projects/ded/test/test_DED.py
+++ python_projects/ded/test/test_DED.py
@@ -46,4 +46,10 @@ class DEDTest(unittest.TestCase):
         result = DED.PUT_LONG(DED, "long", 1)
         self.assertTrue(result > 0, result)
 
+    def testDED_PUT_BOOL(self):
+        DED = ded.DEDEncoder()
+        result = DED.PUT_STRUCT_START(DED, "event")
+        self.assertTrue(result == 1, result)
+        result = DED.PUT_BOOL(DED, "bool", 1)
+        self.assertTrue(result > 0, result)
 
