2dc1ac54e413d000e61191b875534c4c409b09e8
serup <serup@hotmail.com>
2016-02-10 14:46:25 +0100

    ambari NOT support 32bit
    Change-Id: I26d9c7516f9ca4adf5f665b3df332e6c84bf3a01

---
+++ hadoop_projects/hadoopServices/bootstrap-one.sh
@@ -0,0 +1,13 @@
+PLATFORM=$(uname -i)
+if [ "x86_64" != $PLATFORM ]; then
+echo "yum upgrade -- please wait..."
+sudo yum -y clean all
+sudo yum -y check 
+sudo yum -y upgrade 
+echo "install java..."
+sudo yum -y install java
+#sudo wget http://public-repo-1.hortonworks.com/ambari/centos6/1.x/GA/ambari.repo
+#sudo cp ambari.repo /etc/yum.repos.d
+#sudo yum repolist
+#sudo sed -i 's/https/http/g' /etc/yum.repos.d/epel.repo
+fi
