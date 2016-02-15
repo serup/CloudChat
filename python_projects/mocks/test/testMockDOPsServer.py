6e9710ac7d17b670161aac3721513a44c3e47300
serup <serup@hotmail.com>
2016-02-15 10:46:43 +0100

    add test for createprofile in python mockDOPsServer
    Change-Id: Iddc99992d1247ac40dd614179173892f60eaa748

--- python_projects/mocks/test/testMockDOPsServer.py
+++ python_projects/mocks/test/testMockDOPsServer.py
@@ -83,17 +83,16 @@ class DOPsServerTest(unittest.TestCase):
         self.assertEquals(f, 4)
 
     def testCreateProfile(self):
-        trans_id = 123
-        uniqueId = "a12def123"
-        username = "johndoe"
-        password = "abc123"
-
-        # TODO: make a DED package for create profile
+        trans_id = 69
+        # hardcoded user in mock server
+        uniqueId = "985998707DF048B2A796B44C89345494"
+        username = "johndoe@email.com"
+        password = "12345"
 
         DED = ded.DEDEncoder()
         if DED.PUT_STRUCT_START("WSRequest"):
-            DED.PUT_METHOD   ("name",  "PythonConnect")
-            DED.PUT_USHORT   ("trans_id",  trans_id)
+            DED.PUT_METHOD("name",  "PythonConnect")
+            DED.PUT_USHORT("trans_id",  trans_id)
             DED.PUT_STDSTRING("protocolTypeID", "DED1.00.00")
             DED.PUT_STDSTRING("functionName", uniqueId)
             DED.PUT_STDSTRING("username", username)
@@ -111,31 +110,31 @@ class DOPsServerTest(unittest.TestCase):
         DED2 = ded.DEDEncoder()
         DED2.PUT_DATA_IN_DECODER(bytearray(receivedData), len(bytearray(receivedData)))
         # verify that data is inside decoder, and that it has been decompressed correct
-        self.assertTrue(DED2.ptotaldata, DEDobj.uncompresseddata)
+        # self.assertTrue(DED2.ptotaldata, DEDobj.uncompresseddata)  # only if echo
 
         # start decoding
         if DED2.GET_STRUCT_START("WSResponse"):
-                strMethod   = DED2.GET_METHOD ("name" )
-                uTrans_id   = DED2.GET_USHORT ("trans_id")
-                strProtocolTypeID  = DED2.GET_STDSTRING ("protocolTypeID")
-                strFunctionName    = DED2.GET_STDSTRING ("functionName")
-                strStatus  = DED2.GET_STDSTRING ("status")
-                DED2.GET_STRUCT_END( "WSResponse" )
-                bDecoded = True
-                print("DED packet decoded - now validate")
+            strMethod = DED2.GET_METHOD("name")
+            uTrans_id = DED2.GET_USHORT("trans_id")
+            strProtocolTypeID = DED2.GET_STDSTRING("protocolTypeID")
+            strFunctionName = DED2.GET_STDSTRING("functionName")
+            strStatus = DED2.GET_STDSTRING ("status")
+            DED2.GET_STRUCT_END("WSResponse")
+            bDecoded = True
+            print("DED packet decoded - now validate")
 
-                if strMethod != "PythonConnect": bDecoded = False
-                self.assertEquals(True, bDecoded)
-                if uTrans_id != trans_id: bDecoded = False
-                self.assertEquals(True, bDecoded)
-                if strProtocolTypeID != "DED1.00.00": bDecoded = False
-                self.assertEquals(True, bDecoded)
-                if strFunctionName != uniqueId: bDecoded = False
-                self.assertEquals(True, bDecoded)
-                if strStatus != "ACCEPTED": bDecoded = False
-                self.assertEquals(True, bDecoded)
+            if strMethod != "PythonConnect": bDecoded = False
+            self.assertEquals(True, bDecoded)
+            if uTrans_id != trans_id: bDecoded = False
+            self.assertEquals(True, bDecoded)
+            if strProtocolTypeID != "DED1.00.00": bDecoded = False
+            self.assertEquals(True, bDecoded)
+            if strFunctionName != uniqueId: bDecoded = False
+            self.assertEquals(True, bDecoded)
+            if strStatus != "ACCEPTED": bDecoded = False
+            self.assertEquals(True, bDecoded)
 
-                if bDecoded:
-                    print("DED packet validated - OK")
+            if bDecoded:
+                print("DED packet validated - OK")
         else:
-                bDecoded = False
+            bDecoded = False
