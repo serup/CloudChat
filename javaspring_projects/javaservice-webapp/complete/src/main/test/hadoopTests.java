e82ace409c0e48a2aa1d99a22c073ac3a282af99
serup <serup@hotmail.com>
2016-02-19 10:55:39 +0100

    init - hadoop test mapreduce
    Change-Id: I92244fb1c0f19c4f22d05a797cb04501e344a1c0

---
+++ javaspring_projects/javaservice-webapp/complete/src/main/test/hadoopTests.java
@@ -0,0 +1,29 @@
+import hadoop.hadoopMappers.WordMapper;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by serup on 19-02-16.
+ */
+public class hadoopTests {
+
+    @Before
+    public void setupHadoop()
+    {
+
+    }
+
+    @Test
+    public void testCreateMapperComponent()
+    {
+        /**
+         * Creating the Mapper Component
+         * A mapper is a component that divides the original problem into smaller problems that are easier to solve.
+         * - create a custom mapper component by extending the Mapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT> class
+         *   and overriding its map() method.
+         */
+        WordMapper wordMapper = new WordMapper();
+
+
+    }
+}
