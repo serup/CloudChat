3389c3859f4f0073eadb841e4e7b2f76f74223d9
serup <serup@hotmail.com>
2016-03-01 11:06:16 +0100

    deprecated
    Change-Id: I122ed05aaaa0801c5b058e3a18397e64f6c6c419

--- hadoop_projects/hadoopServices/scpToHadoop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source="$1"
-#destination="$2"
-
-#example : sshpass -p "vagrant" scp -oStrictHostKeyChecking=no testscp.txt vagrant@one.cluster:/home/vagrant/.
-#sshpass -p "vagrant" scp -oStrictHostKeyChecking=no $source vagrant@one.cluster:/home/vagrant/$destination/.
-sshpass -p "vagrant" scp -oStrictHostKeyChecking=no $source vagrant@one.cluster:/home/vagrant/.
-
