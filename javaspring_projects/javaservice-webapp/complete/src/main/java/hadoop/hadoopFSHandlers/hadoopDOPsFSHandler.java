32d2be3dcb34f893fafe85e5904183f04271267f
serup <serup@hotmail.com>
2016-03-01 09:46:57 +0100

    boble
    Change-Id: Ib73f3b34478099fd99307083920ad9ac64b44c0c

--- javaspring_projects/javaservice-webapp/complete/src/main/java/hadoop/hadoopFSHandlers/hadoopDOPsFSHandler.java
+++ javaspring_projects/javaservice-webapp/complete/src/main/java/hadoop/hadoopFSHandlers/hadoopDOPsFSHandler.java
@@ -3,7 +3,6 @@ package hadoop.hadoopFSHandlers;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.FsShell;
 import org.apache.hadoop.fs.Path;
 
 import java.io.BufferedReader;
@@ -23,40 +22,14 @@ public class hadoopDOPsFSHandler {
      */
     Configuration conf;
     URI uri;
-    FsShell fileSystem;
     FileSystem fs;
-
-    private String executeMultiCommand(String[] command, String path) {
-
-        StringBuffer output = new StringBuffer();
-        File dir = new File(path);
-
-        // create a process and execute cmd and currect environment
-        try {
-            Process process = Runtime.getRuntime().exec(command, null, dir);
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            process.waitFor();
-
-            String line = "";
-            while ((line = reader.readLine())!= null) {
-                output.append(line + "\n");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            output.append(e.getMessage());
-        }
-
-        return output.toString();
-    }
 
     private String executeCommand(String command, String path) {
 
         StringBuffer output = new StringBuffer();
         File dir = new File(path);
 
-        // create a process and execute cmd and currect environment
+        // create a process and execute cmd
         try {
             Process process = Runtime.getRuntime().exec(command, null, dir);
 
@@ -93,7 +66,6 @@ public class hadoopDOPsFSHandler {
 
     public hadoopDOPsFSHandler() // constructor
     {
-        //fileSystem = new org.apache.hadoop.fs.FsShell();
         conf = new Configuration();
         conf.set("fs.defaultFS", "hdfs://one.cluster:8020/");
         uri = URI.create ("hdfs://one.cluster:8020/");
@@ -142,18 +114,16 @@ public class hadoopDOPsFSHandler {
     public boolean copyTo(String inputfile, String destinationHDFSPath) throws IOException {
         boolean bResult = false;
 
-        // init
-        String path = new File(".").getCanonicalPath();
-        path = trimOffLastFileSeperator(path, 3);
-        path = path.concat("/hadoop_projects/hadoopServices"); // focus on current path
-
-        // first make a scp copy to one.cluster - use script, since sshpass is used and multi cmd from here does NOT work
-        String cmd = "./scpToHadoop.sh " + inputfile;
-        String result = executeCommand(cmd, path);
-
-        // second put file into hdfs at destinationHDFSPath
         String fileName = inputfile.substring( inputfile.lastIndexOf('/')+1, inputfile.length() );
+        // put file into hdfs at destinationHDFSPath
+        fs = FileSystem.get(uri, conf); // fetch filesystem handle for hdfs on one.cluster server
+        Path dest = new Path(destinationHDFSPath);
+        fs.copyFromLocalFile(new Path(inputfile), dest);
 
+        // cleanup - when testing
+        String newfileName = destinationHDFSPath + "/" + fileName;
+        //fs.deleteOnExit(new Path(newfileName));
+        remove(newfileName);
 
         return bResult;
     }
