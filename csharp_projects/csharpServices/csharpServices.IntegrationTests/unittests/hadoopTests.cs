af6cf656070ba36c311a73195d077ad2962927da
serup <serup@hotmail.com>
2016-03-08 14:00:55 +0100

    FEATURE - start work on connecting with hadoop cluster
    Change-Id: I1dfcba18bb8925507d6da6e1d03d3769ceaeda03

---
+++ csharp_projects/csharpServices/csharpServices.IntegrationTests/unittests/hadoopTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using WebSocketClient;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+
+namespace hadoopTests
+{
+	[TestFixture]
+	public class hadoopConnectionTests
+	{
+		[Test]
+		public void connectTohadoop ()
+		{
+			Assert.IsTrue (false);
+		}
+	}
+}
+
