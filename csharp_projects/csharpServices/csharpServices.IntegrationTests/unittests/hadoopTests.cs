d61f4888577cd616d9ad5915bba92f5c45f7c13e
serup <serup@hotmail.com>
2016-03-10 14:31:08 +0100

    FEATURE - add mapper and reduce job
    Change-Id: I75dcf31faa8f125587506630b5ce32caf3f8a317

--- csharp_projects/csharpServices/csharpServices.IntegrationTests/unittests/hadoopTests.cs
+++ csharp_projects/csharpServices/csharpServices.IntegrationTests/unittests/hadoopTests.cs
@@ -19,38 +19,38 @@ namespace HadoopTests
 		{
 			// TODO: use following to make a test for connecting to hadoop VM cluster
 			// http://www.codeguru.com/columns/experts/how-to-create-mapreduce-jobs-for-hadoop-using-c.htm
-			/*
+
 			HadoopJobConfiguration hadoopConfiguration = new HadoopJobConfiguration();  //TODO: require a new version og NuGet Packet Manager to install Microsoft MapReduce hadoop
-	        hadoopConfiguration.InputPath = "/input";
-	        hadoopConfiguration.OutputFolder = "/output";
-	        Uri myUri = new Uri("one.cluster:8020");
-	        IHadoop hadoop = Hadoop.Connect(myUri, "vagrant", "vagrant");
+		        hadoopConfiguration.InputPath = "/input";
+		        hadoopConfiguration.OutputFolder = "/output";
+		        Uri myUri = new Uri("one.cluster:8020");
+		        IHadoop hadoop = Hadoop.Connect(myUri, "vagrant", "vagrant");  // NB! if System.Net.Http fails, then set System.Web and System.Net.Http to copy local -- right click on reference and change
+ 
+	    		hadoop.MapReduceJob.Execute<ErrorTextMapper, ErrorTextReducerCombiner>(hadoopConfiguration);
 	 
-	    //    hadoop.MapReduceJob.Execute<ErrorTextMapper, ErrorTextReducerCombiner>(hadoopConfiguration);
-	 */
 			Assert.IsTrue (false);
 		}
 	}
 
-	/*
+
 	// Mapper and Reduce classes ;  NuGet package Microsoft .Net Map Reduce API for Hadoop
 	public class ErrorTextMapper : MapperBase
-    {
-        public override void Map(string inputLine, MapperContext context)
-        {
-            if (inputLine.ToLowerInvariant().Equals("error"))
-                context.EmitLine(inputLine);
-        }
-    }
+	{
+		public override void Map(string inputLine, MapperContext context)
+		{
+		    if (inputLine.ToLowerInvariant().Equals("error"))
+		        context.EmitLine(inputLine);
+		}
+	}
 
 	public class ErrorTextReducerCombiner : ReducerCombinerBase
-    {
-        public override void Reduce(string key, IEnumerable<string> values, ReducerCombinerContext context)
-        {
-            context.EmitKeyValue("errortextcount: ", values.Count().ToString());
-        }
-    }
+	{
+		public override void Reduce(string key, IEnumerable<string> values, ReducerCombinerContext context)
+		{
+		    context.EmitKeyValue("errortextcount: ", values.Count().ToString());
+		}
+	}
 
-    */
+
 }
 
