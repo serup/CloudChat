4d0d4579311cf964c93ec57a9aed384731f801ae
serup <serup@hotmail.com>
2016-04-01 10:26:10 +0200

    ISSUE - mapreduce job NOT executed
    Change-Id: I44ea14ad53a5389a61b1ca1e272dc84baf1d3659

--- csharp_projects/csharpServices/csharpServices.IntegrationTests/unittests/hadoopTests.cs
+++ csharp_projects/csharpServices/csharpServices.IntegrationTests/unittests/hadoopTests.cs
@@ -10,6 +10,7 @@ using System.Linq;
 
 // MapReduce example: http://www.codeproject.com/Articles/524233/MapReduceplus-fplusMapplusReductionplusStrategies
 using SharpHadoop;
+using System.Text.RegularExpressions;
 
 
 namespace HadoopTests
@@ -52,13 +53,24 @@ namespace HadoopTests
 			// TODO: use following to make a test for connecting to hadoop VM cluster
 			// http://www.codeguru.com/columns/experts/how-to-create-mapreduce-jobs-for-hadoop-using-c.htm
 
-			HadoopJobConfiguration hadoopConfiguration = new HadoopJobConfiguration();  //TODO: require a new version og NuGet Packet Manager to install Microsoft MapReduce hadoop
-		        hadoopConfiguration.InputPath = "/input";
-		        hadoopConfiguration.OutputFolder = "/output";
+			//HadoopJobConfiguration hadoopConfiguration = new HadoopJobConfiguration();  //TODO: require a new version og NuGet Packet Manager to install Microsoft MapReduce hadoop
+		        //hadoopConfiguration.InputPath = "/input";
+		        //hadoopConfiguration.OutputFolder = "/output";
+
+			Environment.SetEnvironmentVariable("HADOOP_HOME", @"/usr/local/hadoop");
+			Environment.SetEnvironmentVariable("JAVA_HOME", @"/usr/lib/jvm/java-8-oracle");
+			Environment.SetEnvironmentVariable("Java_HOME", @"/usr/lib/jvm/java-8-oracle");
+
 		        Uri myUri = new Uri("http://one.cluster:50070");
 		        IHadoop hadoop = Hadoop.Connect(myUri, "admin", "admin");  // NB! if System.Net.Http fails, then set System.Web and System.Net.Http to copy local -- right click on reference and change
 
-	    		hadoop.MapReduceJob.Execute<ErrorTextMapper, ErrorTextReducerCombiner>(hadoopConfiguration);
+//			hadoop.MapReduceJob.Execute<ErrorTextMapper, ErrorTextReducerCombiner>(hadoopConfiguration);
+
+			// first create /tmp/input/CodeFiles
+			// then copy some .cs files to one.cluster local -  sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /home/serup/GerritHub/CloudChat/csharp_projects/csharpServices/csharpServices/*.cs vagrant@one.cluster:/tmp
+			// then copy .cs files into hadoop - 
+			// hadoop fs -copyFromLocal /tmp/*.cs /tmp/input/CodeFiles
+			hadoop.MapReduceJob.ExecuteJob<NamespaceCounterJob>();
 	 
 			//Assert.IsTrue (false);
 		}
@@ -83,6 +95,48 @@ namespace HadoopTests
 		    context.EmitKeyValue("errortextcount: ", values.Count().ToString());
 		}
 	}
+
+	///
+
+	//Mapper 
+	public class NamespaceMapper : MapperBase 
+	{ 
+		//Override the map method. 
+		public override void Map(string inputLine, MapperContext context) 
+		{ 
+			//Extract the namespace declarations in the Csharp files 
+			var reg = new Regex(@"(using)\s[A-za-z0-9_\.]*\;"); 
+			var matches = reg.Matches(inputLine); 
+			foreach (Match match in matches) 
+			{ 
+				//Just emit the namespaces. 
+				context.EmitKeyValue(match.Value,"1"); 
+			} 
+		}
+	} 
+
+	//Reducer 
+	public class NamespaceReducer : ReducerCombinerBase 
+	{ 
+		//Accepts each key and count the occurrances 
+		public override void Reduce(string key, IEnumerable<string> values, ReducerCombinerContext context) 
+		{ 
+			//Write back 
+			context.EmitKeyValue(key,values.Count().ToString()); 
+		} 
+	} //  - See more at: http://www.amazedsaint.com/2013/03/taming-big-data-with-c-using-hadoop-on.html#sthash.dQe0hV2X.dpuf
+
+
+	public class NamespaceCounterJob : HadoopJob<NamespaceMapper, NamespaceReducer> 
+	{ 
+		public override HadoopJobConfiguration Configure(ExecutorContext context) 
+		{ 
+			var config = new HadoopJobConfiguration(); 
+			config.InputPath = "/tmp/input/CodeFiles"; 
+			config.OutputFolder = "/tmp/output/CodeFiles"; 
+			return config; 
+		} 
+	} // - See more at: http://www.amazedsaint.com/2013/03/taming-big-data-with-c-using-hadoop-on.html#sthash.dQe0hV2X.dpuf
 
 
 }
