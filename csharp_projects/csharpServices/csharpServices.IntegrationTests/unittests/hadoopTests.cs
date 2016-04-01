f1b1b8737bfb5ad17a2476f93feef975a216cfe2
serup <serup@hotmail.com>
2016-04-01 13:56:36 +0200

    ISSUE - mapper reduce
    Change-Id: Id63b872547d32a922cfde8e83e92f429241bbc6c

--- csharp_projects/csharpServices/csharpServices.IntegrationTests/unittests/hadoopTests.cs
+++ csharp_projects/csharpServices/csharpServices.IntegrationTests/unittests/hadoopTests.cs
@@ -1,16 +1,8 @@
 ﻿using System;
 using NUnit.Framework;
-using WebSocketClient;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Net.WebSockets;
 using Microsoft.Hadoop.MapReduce;
-using System.Collections.Generic;
-using System.Linq;
-
 // MapReduce example: http://www.codeproject.com/Articles/524233/MapReduceplus-fplusMapplusReductionplusStrategies
 using SharpHadoop;
-using System.Text.RegularExpressions;
 using csharpServices.IntegrationTests;
 
 
