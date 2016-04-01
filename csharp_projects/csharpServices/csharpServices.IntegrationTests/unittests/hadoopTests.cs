5f3d3426f3cf10d9f557eb0037e3a6cc77b0c696
serup <serup@hotmail.com>
2016-04-01 13:07:29 +0200

    ISSUE - mapper reduce
    Change-Id: I2f7db77160a5dd816603206d5732c215566c3a22

--- csharp_projects/csharpServices/csharpServices.IntegrationTests/unittests/hadoopTests.cs
+++ csharp_projects/csharpServices/csharpServices.IntegrationTests/unittests/hadoopTests.cs
@@ -11,6 +11,7 @@ using System.Linq;
 // MapReduce example: http://www.codeproject.com/Articles/524233/MapReduceplus-fplusMapplusReductionplusStrategies
 using SharpHadoop;
 using System.Text.RegularExpressions;
+using csharpServices.IntegrationTests;
 
 
 namespace HadoopTests
@@ -57,47 +58,16 @@ namespace HadoopTests
 		        Uri myUri = new Uri("http://one.cluster:50070");
 		        IHadoop hadoop = Hadoop.Connect(myUri, "admin", "admin");  // NB! if System.Net.Http fails, then set System.Web and System.Net.Http to copy local -- right click on reference and change
 
-//			hadoop.MapReduceJob.Execute<ErrorTextMapper, ErrorTextReducerCombiner>(hadoopConfiguration);
-
 			// first create /tmp/input/CodeFiles
 			// then copy some .cs files to one.cluster local -  sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /home/serup/GerritHub/CloudChat/csharp_projects/csharpServices/csharpServices/*.cs vagrant@one.cluster:/tmp
 			// then copy .cs files into hadoop - 
 			// hadoop fs -copyFromLocal /tmp/*.cs /tmp/input/CodeFiles
+			// - prerequisite
+			// NB! make sure MRLib is copied into CloudChat/csharp_projects/csharpServices/csharpServices.IntegrationTests/bin/Debug/MRLib
+			// cp -r CloudChat/csharp_projects/csharpServices/csharpServices.IntegrationTests/net40/MRLib ../../bin/Debug/.
 			hadoop.MapReduceJob.ExecuteJob<NamespaceCounterJob>();  // http://www.amazedsaint.com/2013/03/taming-big-data-with-c-using-hadoop-on.html
-	 
-			//Assert.IsTrue (false);
 		}
 	}
-
-
-
-	//Mapper 
-	public class NamespaceMapper : MapperBase 
-	{ 
-		//Override the map method. 
-		public override void Map(string inputLine, MapperContext context) 
-		{ 
-			//Extract the namespace declarations in the Csharp files 
-			var reg = new Regex(@"(using)\s[A-za-z0-9_\.]*\;"); 
-			var matches = reg.Matches(inputLine); 
-			foreach (Match match in matches) 
-			{ 
-				//Just emit the namespaces. 
-				context.EmitKeyValue(match.Value,"1"); 
-			} 
-		}
-	} 
-
-	//Reducer 
-	public class NamespaceReducer : ReducerCombinerBase 
-	{ 
-		//Accepts each key and count the occurrances 
-		public override void Reduce(string key, IEnumerable<string> values, ReducerCombinerContext context) 
-		{ 
-			//Write back 
-			context.EmitKeyValue(key,values.Count().ToString()); 
-		} 
-	} //  - See more at: http://www.amazedsaint.com/2013/03/taming-big-data-with-c-using-hadoop-on.html#sthash.dQe0hV2X.dpuf
 
 
 	public class NamespaceCounterJob : HadoopJob<NamespaceMapper, NamespaceReducer> 
