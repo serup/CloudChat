94848d5bf38d942027847eac6ff59d3fd214358d
serup <serup@hotmail.com>
2016-04-04 09:45:30 +0200

    FEATURE [hadoop;JavaSpring;Test:WordCount] - INIT
    Change-Id: Iff6b37814ff01dad19f122e36fe28994ead9cced

--- javaspring_projects/javaservice-webapp/complete/src/main/java/hello/Application.java
+++ javaspring_projects/javaservice-webapp/complete/src/main/java/hello/Application.java
@@ -2,11 +2,14 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
         SpringApplication.run(Application.class, args);
     }
 }
