94957374034dd872db819d7a65a2add46d0b2002
serup <serup@hotmail.com>
2016-04-26 09:39:38 +0200

    BUGFIX - conversion issue
    Change-Id: I5e53f74fe3f9e4094c42d562b8f243317b311e0f

--- javaspring_projects/javaservice-webapp/complete/src/main/java/dops/hadoop/reducers/ProfileFileReducer.java
+++ javaspring_projects/javaservice-webapp/complete/src/main/java/dops/hadoop/reducers/ProfileFileReducer.java
@@ -18,15 +18,24 @@ import java.io.StringReader;
  *
  * INPUT IS A XML structure
  *
- * <ProfileRecord>
- *     <TransGUID>581dd417e98c4e11b3b6aa15435cd929</TransGUID>
+ *  <Entity>
+ *   <version>1</version>
+ *   <ProfileRecord>
+ *     <TransGUID>581dd417e98c4e11b3b6aa15435cd929</TransGUID> -- each record has a unique transaction id to be used with security, when detecting inconsistencies
  *     <Protocol>DED</Protocol>
  *     <ProtocolVersion>1.0.0.0</ProtocolVersion>
- *     <DataSize>492</DataSize>
- *     <Data>...</Data>
- *     <DataMD5>EB11373F99512CD9731B74621A9F6B15</DataMD5>
- * </ProfileRecord>
- *
+ *     <DataSize></DataSize>
+ *     <Data>
+ *          <record>
+ *              <chunk_id></chunk_id>
+ *              <aiid></aiid>
+ *              <chunk_seq></chunk_seq>
+ *              <chunk_data></chunk_data>
+ *          </record>
+ *     </Data> -- here is the pCompressedData from above protocol encoding, compression, encrypting algoritm -- currently DED (dataencoderdecoder)
+ *     <DataMD5>EB11373F99512CD9731B74621A9F6B15<DataMD5> -- this md5 checksum is used as an extra verification -- Data must be verified with this value before being used.
+ *   </ProfileRecord>
+ * </Entity>
  *
  * output result:
  *
