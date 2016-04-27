96e75bfc07bb8dad55e3fc93690dddb91c486b24
serup <serup@hotmail.com>
2016-04-27 13:06:42 +0200

    FEATURE [JavaSpring;Test;IntegrationTest:testFindProfileFile] - REFACTOR
    Change-Id: Iaf66c7db92da16f4c109c13c806f1dbb6f63d439

--- javaspring_projects/javaservice-webapp/complete/src/main/test/integrationTests/dops/hadoop/DOPsMRTests.java
+++ javaspring_projects/javaservice-webapp/complete/src/main/test/integrationTests/dops/hadoop/DOPsMRTests.java
@@ -22,38 +22,33 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.mortbay.util.LazyList.contains;
 
-/**
- * Created by serup on 26-04-16.
- */
 public class DOPsMRTests {
 
-    DOPsHDFSHandler fshandlerDriver;
+    private DOPsHDFSHandler fshandlerDriver;
+    private final IntegrationEnvironmentSetup env = new IntegrationEnvironmentSetup();
 
-
-    /*
-    */
-    IntegrationEnvironmentSetup env = new IntegrationEnvironmentSetup();
-
+    @SuppressWarnings("unused")
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         assertEquals(true,env.setupHadoopIntegrationEnvironment());
+        try {
         fshandlerDriver = new DOPsHDFSHandler();
 
         File directory = new File("/tmp/output/findprofile");
         //make sure directory exists
         if (!directory.exists()) {
             // Directory does not exist
-            directory.mkdir();
-        }else {
-            try {
-                env.delete(directory); // clean out old
-                directory.mkdir(); // create for this test
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(0);
-            }
+            if(!directory.mkdir())
+                    throw new IOException("Could NOT create folder: /tmp/output/findprofile");
         }
-
+        else {
+                env.delete(directory); // clean out old
+                if(!directory.mkdir())
+                    throw new IOException("Could NOT create folder: /tmp/output/findprofile"); // create for this test
+        }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -74,7 +69,7 @@ public class DOPsMRTests {
             conf.setBoolean("mapreduce.map.speculative", false);
             conf.setBoolean("mapreduce.reduce.speculative", false);
 
-            Job job = null;
+            Job job;
             job = Job.getInstance(conf);
             job.setJobName("FindProfileFile");
             job.setJarByClass(ProfileFileMapper.class);
@@ -92,42 +87,45 @@ public class DOPsMRTests {
             String destFolder="tmp/input/findprofile";
             URL fileResourceUrl = this.getClass().getClassLoader().getResource(fileResource1);
             URL fileResourceUrl2 = this.getClass().getClassLoader().getResource(fileResource2);
-            fshandlerDriver.copyTo(fileResourceUrl.getPath(), "/"+destFolder+"/355760fb6afaf9c41d17ac5b9397fd45.xml");
-            fshandlerDriver.copyTo(fileResourceUrl2.getPath(), "/"+destFolder+"/toast"+"/355760fb6afaf9c41d17ac5b9397fd45_toast.xml");
+            if(fileResourceUrl!=null) {
+                fshandlerDriver.copyTo(fileResourceUrl.getPath(), "/" + destFolder + "/355760fb6afaf9c41d17ac5b9397fd45.xml");
+                if (fileResourceUrl2 != null)
+                    fshandlerDriver.copyTo(fileResourceUrl2.getPath(), "/" + destFolder + "/toast" + "/355760fb6afaf9c41d17ac5b9397fd45_toast.xml");
+                else
+                    throw new IOException("Resource file NOT present");
+            }
+            else
+                throw new IOException("Resource file NOT present");
 
             // verify that the input file is in hdfs
-            List<String> itemsToAdd = new ArrayList<String>();
+            List<String> itemsToAdd = new ArrayList<>();
             String filepathname1 = destFolder+"/"+"355760fb6afaf9c41d17ac5b9397fd45.xml";
             String filepathname2 = destFolder+"/toast/"+"355760fb6afaf9c41d17ac5b9397fd45_toast.xml";
             itemsToAdd.add(fshandlerDriver.uri+filepathname1);
             itemsToAdd.add("hdfs://one.cluster:8020/tmp/input/findprofile/toast");
             assertEquals("Expected 2 item in hdfs ls list", itemsToAdd, fshandlerDriver.ls("/tmp/input/findprofile/"));
-            itemsToAdd = new ArrayList<String>();
+            itemsToAdd = new ArrayList<>();
             itemsToAdd.add(fshandlerDriver.uri+filepathname2);
             assertEquals("Expected 1 item in hdfs ls list", itemsToAdd, fshandlerDriver.ls("/tmp/input/findprofile/toast"));
 
             job.waitForCompletion(true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+
+            // This should result in a result file on LOCAL file system
+            // see following example:
+            // serup@serup-ThinkPad-T440s:~$ ls /tmp/output/findprofile/result/
+            // part-r-00000  _SUCCESS
+            // serup@serup-ThinkPad-T440s:~$ cat /tmp/output/findprofile/result/part-r-00000
+            // <result>\t\n<file>355760fb6afaf9c41d17ac5b9397fd45.xml</file>\t\n</result>\t\n
+            //
+            // above shows that the field searched for was found in <file>
+
+            String result = env.executeCmd("cat /tmp/output/findprofile/result/part-r-00000", "/");
+            assertEquals(true, contains(result, "<result>\t\n<file>355760fb6afaf9c41d17ac5b9397fd45.xml</file>\t\n</result>\t\n"));
+
+            System.out.println("FindProfileFile - mapreduce job finished - status : SUCCESS");
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-        // This should result in a result file on LOCAL file system
-        // see following example:
-        // serup@serup-ThinkPad-T440s:~$ ls /tmp/output/findprofile/result/
-        // part-r-00000  _SUCCESS
-        // serup@serup-ThinkPad-T440s:~$ cat /tmp/output/findprofile/result/part-r-00000
-        // <result>\t\n<file>355760fb6afaf9c41d17ac5b9397fd45.xml</file>\t\n</result>\t\n
-        //
-        // above shows that the field searched for was found in <file>
-
-        String result = env.executeCmd("cat /tmp/output/findprofile/result/part-r-00000", "/");
-        assertEquals(true, contains(result, new String("<result>\t\n<file>355760fb6afaf9c41d17ac5b9397fd45.xml</file>\t\n</result>\t\n")));
-
-        System.out.println("FindProfileFile - mapreduce job finished - status : SUCCESS");
     }
 
 }
