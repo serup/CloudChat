df1d4f5dcded1ec01025c29ff4b571ced8f7a9ca
serup <serup@hotmail.com>
2016-04-28 08:08:03 +0200

    apt-fast issue
    Change-Id: I31a9cfaff3caf6f5fdc829f873bdaa91dabbfadd

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ if [ "" == "$PKG_OK" ]; then
   echo -n "- install apt-fast on ubuntu, to use for speeding up apt-get install's "
   sudo add-apt-repository ppa:saiarcot895/myppa
   sudo apt-get update
-  sudo apt-fast install -yq apt-fast
+  sudo apt-get install -yq apt-fast
   echo " - done."
 else
   echo "- apt-fast already installed"
