eb78b3a037199e79bbd0d41fb58f63b49bf6d4bd
serup <serup@serup-VBox-u16.04>
2016-05-03 14:36:29 +0200

    ADD - check for hadoop cluster running outside VM
    Change-Id: I69cd07ab0a142a777f5caaa814063ec014fecca6

--- javaspring_projects/javaservice-webapp/complete/src/test/java/integrationTests/IntegrationEnvironmentSetup.java
+++ javaspring_projects/javaservice-webapp/complete/src/test/java/integrationTests/IntegrationEnvironmentSetup.java
@@ -74,11 +74,12 @@ public class IntegrationEnvironmentSetup {
         boolean bResult=true;
         if (!bIntegrationEnvironmentAlreadySetup) {
             try {
-                // Make sure VM is running for this test
-                System.out.println("Waiting for VM to start ...");
-
                 String path = new File(".").getCanonicalPath();
                 path = trimOffLastFileSeperator(path, 3);
+
+
+                // Make sure VM is running for this test
+                System.out.println("Waiting for VM to start ...");
 
                 String cmd = "vagrant --version";
                 String result = executeCommand(cmd, path);
@@ -106,36 +107,43 @@ public class IntegrationEnvironmentSetup {
         boolean bResult=true;
         if (!bHadoopIntegrationEnvironmentAlreadySetup) {
             try {
-                // Make sure VM is running for this test
-                System.out.println("Waiting for VM hadoop one.cluster to start ... - perhaps you should start a minimum of three yourself, and make sure ambari-server and ambari-agent on all nodes is running - please " +
-                        "wait it " +
-                        "takes a long time...");
-                System.out.println("NB! DO NOT use vagrant halt, when shutting down a cluster - use vagrant suspend and vagrant resume - otherwise you will have to use ambari-server one.cluster:8080 admin admin to " +
-                        "reestablish connection between all nodes in cluster - its takes long time");
+                System.out.println("Check if main node in hadoop cluster is running...");
                 String path = new File(".").getCanonicalPath();
                 path = trimOffLastFileSeperator(path, 3);
-                path = path.concat("/hadoop_projects/hadoopServices");
-
-                String cmd = "vagrant --version";
+                String cmd = "ping one.cluster -c 1";
                 String result = executeCommand(cmd, path);
-                assertEquals(true, result.contains("Vagrant"));
+                if(!result.contains("1 received")) {
 
-                cmd = "vagrant status one.cluster";
-                result = executeCommand(cmd, path);
-                if (result.isEmpty() || !result.contains("running"))
-                {
-                    // cmd = "vagrant up"; // will start all nodes described in Vagrantfile
-                    cmd = "vagrant up one"; // will start 1 nodes
+                    // Make sure VM is running for this test
+                    System.out.println("Waiting for VM hadoop one.cluster to start ... - perhaps you should start a minimum of three yourself, and make sure ambari-server and ambari-agent on all nodes is running - please " +
+                            "wait it " +
+                            "takes a long time...");
+                    System.out.println("NB! DO NOT use vagrant halt, when shutting down a cluster - use vagrant suspend and vagrant resume - otherwise you will have to use ambari-server one.cluster:8080 admin admin to " +
+                            "reestablish connection between all nodes in cluster - its takes long time");
+                    path = path.concat("/hadoop_projects/hadoopServices");
+
+                    cmd = "vagrant --version";
                     result = executeCommand(cmd, path);
-                    assertEquals(true, containsAny(result, new String[]{"VM is already running", "Machine booted and ready"}));
-                    cmd = "vagrant up two"; // will start 1 nodes
+                    assertEquals(true, result.contains("Vagrant"));
+
+                    cmd = "vagrant status one.cluster";
                     result = executeCommand(cmd, path);
-                    assertEquals(true, containsAny(result, new String[]{"VM is already running", "Machine booted and ready"}));
-                     System.out.println("VM hadoop cluster is started - now run test");
+                    if (result.isEmpty() || !result.contains("running")) {
+                        // cmd = "vagrant up"; // will start all nodes described in Vagrantfile
+                        cmd = "vagrant up one"; // will start 1 nodes
+                        result = executeCommand(cmd, path);
+                        assertEquals(true, containsAny(result, new String[]{"VM is already running", "Machine booted and ready"}));
+                        cmd = "vagrant up two"; // will start 1 nodes
+                        result = executeCommand(cmd, path);
+                        assertEquals(true, containsAny(result, new String[]{"VM is already running", "Machine booted and ready"}));
+                        System.out.println("VM hadoop cluster is started - now run test");
+                    } else {
+                        System.out.println("VM hadoop cluster already started - now run test");
+                    }
                 }
-                else {
-                    System.out.println("VM hadoop cluster already started - now run test");
-                }
+                else
+                    System.out.println("hadoop cluster is running - outside virtualbox, thus no need for vagrant up - now run test ");
+
             }catch(Exception e){
                 e.printStackTrace();
                 bResult = false;
