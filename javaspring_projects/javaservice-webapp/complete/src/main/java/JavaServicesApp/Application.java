b4366ddf973e67dc03b5e97547e36388d7a45490
serup <serup@hotmail.com>
2016-05-04 16:05:02 +0200

    boble
    Change-Id: Ie57cd2f6f71db6444cbe8eb503dfe30500faa251

--- javaspring_projects/javaservice-webapp/complete/src/main/java/JavaServicesApp/Application.java
+++ javaspring_projects/javaservice-webapp/complete/src/main/java/JavaServicesApp/Application.java
@@ -8,5 +8,8 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);  // https://spring.io/guides/gs/spring-boot/
+
+        //TODO: connect to DOPS and login to DFD as HadoopJavaServiceApp
+        
     }
 }
