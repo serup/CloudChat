f52fd23c9d50eadfd534ea64cc614861b41b0070
johnny serup <serup@hotmail.com>
2016-05-13 13:20:09 +0200

    ADD - color themes to C::B
    Change-Id: I558afb7b81e718b1130a13acb2fd6cca514ecfc6

--- linux-install-codeblocks-ide.sh
+++ linux-install-codeblocks-ide.sh
@@ -7,4 +7,5 @@ if [ "" == "$PKG_OK" ]; then
         sudo add-apt-repository ppa:damien-moore/codeblocks-stable
         sudo apt-get update
         sudo apt-get install codeblocks 
+        sudo cp codeblocks_projects/colour_themes.conf ~/.config/codeblocks/default.conf
 fi
