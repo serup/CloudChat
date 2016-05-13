b164879a5929ddae40b7c0f732b7b39c4154f09f
johnny serup <serup@hotmail.com>
2016-05-13 15:01:15 +0200

    BUGFIX - hadoop nodes could not be installed using puppet
    Change-Id: I2062864c5de001c43ee77edebcc35db5ca47fd00

--- hadoop_projects/hadoopServices/bootstrap-two.sh
+++ hadoop_projects/hadoopServices/bootstrap-two.sh
@@ -20,5 +20,13 @@ fi
 export PATH=${JAVA_HOME}/bin:${PATH}
 export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
 
+PKG_OK=$(sudo yum repolist|grep ambari)
+if [ "" == "$PKG_OK" ]; then
+        echo "WARNING - puppet should have installed - now use script instead"
+        sudo wget http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.2.3.7/ambari.repo
+        sudo cp ambari.repo /etc/yum.repos.d
+        sudo yum -y install ambari-agent
+fi
+
 echo "start ambari agent"
 sudo ambari-agent start
