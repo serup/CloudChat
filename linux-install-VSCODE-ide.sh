ae5669e752c293ed63bb951bd138f00e463e41a6
johnny serup <serup@hotmail.com>
2016-05-18 15:02:56 +0200

    BUGFIX - install newest version
    Change-Id: If6ac00f07e1456a1f755540fc7651c54df45dfda

--- linux-install-VSCODE-ide.sh
+++ linux-install-VSCODE-ide.sh
@@ -1,7 +1,7 @@
 # Specify IDEA version here.  -- NB! MUST RUN as administrator
-VSCODE_PACKAGE=VSCode-linux64.zip
+VSCODE_PACKAGE=VSCode-linux-x64-stable.zip
 
-wget -O "/tmp/$VSCODE_PACKAGE" https://az764295.vo.msecnd.net/public/0.10.1-release/$VSCODE_PACKAGE
+wget -O "/tmp/$VSCODE_PACKAGE" https://az764295.vo.msecnd.net/stable/def9e32467ad6e4f48787d38caf190acbfee5880/$VSCODE_PACKAGE
 cd /tmp
 
 if [ -f "/tmp/$VSCODE_PACKAGE" ]; then
@@ -9,14 +9,13 @@ if [ -f "/tmp/$VSCODE_PACKAGE" ]; then
 
   VSCODE_EXTRACTED_FOLDER=`ls -dt VSCode* | head -1`
   sudo rsync -av "$VSCODE_EXTRACTED_FOLDER" /opt/vscode
-  sudo cp /opt/vscode/VSCode-linux-x64/resources/app/resources/linux/vscode.png /usr/share/pixmaps/
-
+  sudo cp $VSCODE_EXTRACTED_FOLDER/resources/app/resources/linux/code.png /usr/share/pixmaps/
 
   echo '[Desktop Entry]
 Name=VSCODE IDEA
 Type=Application
-Exec=/opt/vscode/VSCode-linux-x64/Code
-Icon=/opt/vscode/VSCode-linux-x64/resources/app/resources/linux/vscode.png
+Exec=/opt/vscode/VSCode-linux-x64/bin/code
+Icon=/opt/vscode/VSCode-linux-x64/resources/app/resources/linux/code.png
 Terminal=false
 Comment=Integrated Development Environment
 NoDisplay=false
@@ -27,7 +26,8 @@ Name[en]=VSCODE IDEA
   sudo desktop-file-install vscode.desktop
   sudo rm -f vscode.desktop
   sudo rm -f /usr/local/bin/code
-  sudo ln -s /opt/vscode/VSCode-linux-x64/Code /usr/local/bin/code
+  sudo ln -s /opt/vscode/VSCode-linux-x64/bin/code /usr/local/bin/code
+
 else
   echo "error"
 fi
