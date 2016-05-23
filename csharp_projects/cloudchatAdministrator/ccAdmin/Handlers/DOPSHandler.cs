f9551c00b08777113f544235a14d495971425e48
johnny serup <serup@hotmail.com>
2016-05-23 13:10:30 +0200

    PROJECT[cloudchatAdministrator; FEATURE: handleConnectionWithServer]
    Change-Id: I1d5ad3d999c2126e7c23480df04b335ec47ee813

---
+++ csharp_projects/cloudchatAdministrator/ccAdmin/Handlers/DOPSHandler.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using WebSocketClient;
+using DED;
+using System.Threading;
+
+namespace csharpServices
+{
+	public class DOPSHandler
+	{
+		private Client.wshandles _handles;
+		private short trans_id;
+		private String uniqueId;
+		private bool bConnected = false;
+
+		public bool connectToDOPsServer()
+		{
+			bool bResult = false;
+			if (connectToDOPsServer("ws://backend.scanva.com:7777")) {
+				bConnected = true;
+				sendRequestToServer(createLoginRequest());
+				switch (handleServerReply(waitForServerReply())) {
+				case "Request accepted":
+					bResult = true;
+					break;
+				case "Request denied":
+					break;
+				}
+			}
+			return bResult;
+		}
+
+		private bool connectToDOPsServer(string url)
+		{
+			bool bResult = false;
+			_handles = Client.WSConnect ("ws://backend.scanva.com:7777");
+			if(_handles != null) {
+				if(_handles.webSocket.State == System.Net.WebSockets.WebSocketState.Connecting) {
+					Thread.Sleep(2000); // wait a little time
+					if(_handles.webSocket.State == System.Net.WebSockets.WebSocketState.Connecting) {
+						// Something is wrong - disconnect to try again later
+						disconnectFromDOPsServer();
+						bResult = false;
+					}
+					else
+						bResult = true;
+				} else
+					bResult = true;
+			}
+
+			return bResult;
+		}
+
+		private void sendRequestToServer(byte[] request)
+		{
+			Client.SendBLOB (request, _handles.webSocket).Wait (); 
+		}
+
+		private byte[] createLoginRequest()
+		{
+			/**
+         	* prepare data for the request
+         	*/
+			trans_id = 69;
+			uniqueId = "985998707DF048B2A796B44C89345494";
+			String username = "johndoe@email.com"; // TODO: find a way to safely handle retrieval of username,password - should NOT be stored in source code
+			String password = "12345";
+
+			/**
+         	* create DED request datapacket for DOPS for csharp clients
+         	*/
+			DEDEncoder DED = new DEDEncoder ();
+			DED.PUT_STRUCT_START ("WSRequest");
+			DED.PUT_METHOD ("Method", "CSharpConnect");
+			DED.PUT_USHORT ("TransID", trans_id);
+			DED.PUT_STDSTRING ("protocolTypeID", "DED1.00.00");
+			DED.PUT_STDSTRING ("functionName", uniqueId);
+			DED.PUT_STDSTRING ("username", username);
+			DED.PUT_STDSTRING ("password", password);
+			DED.PUT_STRUCT_END ("WSRequest");
+
+			/**
+			 * return the constructed request
+			 */ 
+			byte[] byteArray = DED.GET_ENCODED_BYTEARRAY_DATA ();
+			return byteArray;
+		}
+
+		private byte[] waitForServerReply()
+		{
+			byte[] receivedData = null;
+			receivedData = Client.FetchReceived (_handles);
+			return receivedData;
+		}
+
+		/**
+		 * handleServerReply 
+		 * 
+		 * return values :
+		 * 	"Request accepted"
+		 * 	"Request denied"
+		 */
+		private string handleServerReply(byte[] receivedData)
+		{
+			string returnResult = "";
+
+			/**
+         	* decode incomming data
+         	*/
+			bool bDecoded = false;
+			String strMethod = "";
+			String strProtocolTypeID = "";
+			String strFunctionName = "";
+			String strStatus = "";
+			String strDest;
+			String strSrc;
+			short uTrans_id = 0;
+
+			// decode data ...
+			DEDDecoder DED2 = new DEDDecoder ();
+			DED2.PUT_DATA_IN_DECODER (receivedData, receivedData.Length);
+			if (DED2.GET_STRUCT_START ("WSResponse") == 1 &&
+			    (strMethod = DED2.GET_METHOD ("Method")).Equals ("1_1_6_LoginProfile")) {
+				// DFD was online and responded - now validate the response
+				if ((uTrans_id = DED2.GET_USHORT ("TransID")) != -1 &&
+				    (strProtocolTypeID = DED2.GET_STDSTRING ("protocolTypeID")).Length > 0 &&
+				    (strDest = DED2.GET_STDSTRING ("dest")).Length > 0 &&
+				    (strSrc = DED2.GET_STDSTRING ("src")).Equals ("DFD_1.1") &&
+				    (strStatus = DED2.GET_STDSTRING ("status")).Length > 0 &&
+				    DED2.GET_STRUCT_END ("DFDResponse") == 1) {
+					if (strDest.Equals (uniqueId) && strSrc.Equals ("DFD_1.1")) {
+						Console.WriteLine ("DED packet validated - OK");
+						returnResult = "Request accepted";
+					} else {
+						bDecoded = false;
+						Console.WriteLine ("DED packet validated - FAILED - DED was NOT a valid DFDResponse");
+						returnResult = "Request denied";
+					}
+				}
+			} else {
+				if ((uTrans_id = DED2.GET_USHORT ("TransID")) != -1 &&
+				    (strProtocolTypeID = DED2.GET_STDSTRING ("protocolTypeID")).Length > 0 &&
+				    (strFunctionName = DED2.GET_STDSTRING ("functionName")).Length > 0 &&
+				    (strStatus = DED2.GET_STDSTRING ("status")).Length > 0 &&
+				    DED2.GET_STRUCT_END ("WSResponse") == 1) {
+					bDecoded = true;
+					Console.WriteLine ("DED packet decoded - now validate");
+
+					if (!strMethod.Equals ("CSharpConnect"))
+						bDecoded = false;
+					if (uTrans_id != trans_id)
+						bDecoded = false;
+					if (!strFunctionName.Equals (uniqueId))
+						bDecoded = false;
+					if (!strStatus.Equals ("ACCEPTED"))
+						bDecoded = false;
+					if (!strProtocolTypeID.Equals ("DED1.00.00"))
+						bDecoded = false;
+
+					if (bDecoded) {
+						Console.WriteLine ("DED packet validated - OK");
+						returnResult = "Request accepted";
+					}
+				} else {
+					bDecoded = false;
+					Console.WriteLine ("DED packet validated - FAILED - DED was NOT a valid WSResponse");
+					returnResult = "Request denied";
+				}
+			}
+
+			return returnResult;
+		}
+
+		public bool isConnected()
+		{
+			return bConnected;
+		}
+		
+		public void disconnectFromDOPsServer()
+		{
+			// Disconnect from server
+			Client.WSDisconnect (_handles);
+			bConnected = false;
+		}
+
+
+
+	}
+
+}
--- csharp_projects/cloudchatAdministrator/ccAdmin/MainWindow.cs
+++ csharp_projects/cloudchatAdministrator/ccAdmin/MainWindow.cs
@@ -2,9 +2,12 @@
 using Gtk;
 using System.Threading.Tasks;
 using System.Threading;
+using csharpServices;
 
 public partial class MainWindow: Gtk.Window
 {
+	DOPSHandler dopsHandler = new DOPSHandler();
+	Label status = new Label();
 	protected Boolean bIsConnected=false;
 	public MainWindow () : base (Gtk.WindowType.Toplevel)
 	{
@@ -17,26 +20,46 @@ public partial class MainWindow: Gtk.Window
 		a.RetVal = true;
 	}
 
+	protected void handleConnectionWithServer(object o, bool bIsConnected)
+	{
+		new Task(() => {
+			if(dopsHandler.connectToDOPsServer())
+			{
+				((Gtk.Action)o).StockId = "gtk-disconnect"; // change icon indicating a connection is established and if pressing again then a disconnect will happen
+				((Gtk.Action)o).ShortLabel = "disconnect";
+				this.UpdateStatusBarText("Communication with DOPs SERVER is established");
+				Thread.Sleep(2000);
+				((Gtk.Action)o).ShortLabel = "";
+			}
+			else {
+				// connection failed - perhaps try again later
+				((Gtk.Action)o).ShortLabel = "";
+				((Gtk.Action)o).StockId = "gtk-connect"; // change icon indicating a connection is established and if pressing again then a disconnect will happen
+				((Gtk.Action)o).Tooltip = "Will connect to DOPs server - allowing cloud chat administrator to communicate with cloud chat managers and cloudchat clients";
+				this.UpdateStatusBarText("Failed to communicate with DOPs SERVER");
+			}
+		}).Start();
+	}
+
+	private void UpdateStatusBarText(string text)
+	{
+		var contextId = this.statusbar6.GetContextId("info");
+		this.statusbar6.Push(contextId, text );
+	}
+
 	protected void OnConnectActionActivated (object o, EventArgs args)
 	{
-		//TODO: check if connection with DOPS exists and if it does then disconnect, otherwise connect
+		bIsConnected = dopsHandler.isConnected();
 		if (!bIsConnected) {
-			bIsConnected = true;
 			((Gtk.Action)o).StockId = "gtk-stop"; // change icon indicating a connection is established and if pressing again then a disconnect will happen
 			((Gtk.Action)o).ShortLabel = " please wait...";
-			new Task(() =>
-				{
-					Thread.Sleep(2000);
-					((Gtk.Action)o).StockId = "gtk-disconnect"; // change icon indicating a connection is established and if pressing again then a disconnect will happen
-					((Gtk.Action)o).ShortLabel = "disconnect";
-					Thread.Sleep(2000);
-					((Gtk.Action)o).ShortLabel = "";
-					((Gtk.Action)o).Tooltip = "Will disconnect from DOPs server";
-				}).Start();
+			handleConnectionWithServer(o, bIsConnected);
 		} else {
 			bIsConnected = false;
+			dopsHandler.disconnectFromDOPsServer();
 			((Gtk.Action)o).StockId = "gtk-connect"; // change icon indicating a connection is established and if pressing again then a disconnect will happen
 			((Gtk.Action)o).Tooltip = "Will connect to DOPs server - allowing cloud chat administrator to communicate with cloud chat managers and cloudchat clients";
+			this.UpdateStatusBarText("DOPs SERVER is disconnected");
 		}
 	}
 
