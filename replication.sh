3895cce14457d0dda762fc2df07308472346976b
jenkins <jenkins@cloud1scanvacom>
2016-05-29 20:25:12 +0200

    boble - alternate transfer logic
    Change-Id: I22307372f374860c1ed35888364a74c5901cec85

--- replication.sh
+++ replication.sh
@@ -2,6 +2,7 @@
 # First resize to a maximum of 200x200 pixels
 mogrify -resize 200x200 *.jpg
 # Then transfer files to frontend server
+#sshpass -p "<password>" scp -rp /var/www/img/ cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/.
 sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/html/CloudChatManager/. 
 #sleep 1000
 # Cleanup
