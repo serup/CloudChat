2655ec8fa79b1e89eea1642b967910b91f54e06c
jenkins <jenkins@cloud1scanvacom>
2016-05-29 21:00:58 +0200

    BUGFIX - oStrict was needed
    Change-Id: Ic3a74508e8fdb1439cd9cdcd74f7f75dc224014b

--- replication.sh
+++ replication.sh
@@ -2,7 +2,8 @@
 # First resize to a maximum of 200x200 pixels
 mogrify -resize 200x200 *.jpg
 # Then transfer files to frontend server
-#sshpass -p "<password>" scp -rp /var/www/img/ cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/.
+#did not work -- sshpass -p "<password>" scp -rp /var/www/img/ cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/.
+#sshpass -p "<password>" scp -oStrictHostKeyChecking=no -rp /var/www/img/* cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/img/.
 sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/html/CloudChatManager/. 
 #sleep 1000
 # Cleanup
