99a5496cf83fb15cd404b3f8a1da9a36a718d23f
johnny serup <serup@hotmail.com>
2016-05-31 14:54:14 +0200

    boble - nodeview work - init
    Change-Id: Id0997d200a8ce3ff14740309192f9567d8c944ad

--- csharp_projects/cloudchatAdministrator/ccAdmin/MainWindow.cs
+++ csharp_projects/cloudchatAdministrator/ccAdmin/MainWindow.cs
@@ -12,12 +12,117 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow () : base (Gtk.WindowType.Toplevel)
 	{
 		Build();
+
+		// Create a column for the name
+		Gtk.TreeViewColumn nameColumn = new Gtk.TreeViewColumn ();
+		nameColumn.Title = "Name";
+
+		// Create a column for the song title
+		Gtk.TreeViewColumn sColumn = new Gtk.TreeViewColumn ();
+		sColumn.Title = "Song Title";
+
+		// Add the columns to the TreeView
+		this.nodeviewManagers.NodeSelection.NodeView.AppendColumn(nameColumn);
+		this.nodeviewManagers.NodeSelection.NodeView.AppendColumn(sColumn);
+
+		// Create a model that will hold two strings -  Name and Song Title
+		Gtk.ListStore mListStore = new Gtk.ListStore (typeof (string), typeof (string));
+
+		// Assign the model to the TreeView
+		this.nodeviewManagers.NodeSelection.NodeView.Model = mListStore;
+
+		// Add some data to the store
+		mListStore.AppendValues ("Garbage", "Dog New Tricks");
+
+		// Create the text cell that will display the artist name
+		Gtk.CellRendererText NameCell = new Gtk.CellRendererText ();
+
+		// Add the cell to the column
+		nameColumn.PackStart (NameCell, true);
+
+		// Do the same for the song title column
+		Gtk.CellRendererText sTitleCell = new Gtk.CellRendererText ();
+		sColumn.PackStart (sTitleCell, true);
+
+		// Tell the Cell Renderers which items in the model to display
+		nameColumn.AddAttribute (NameCell, "text", 0);
+		sColumn.AddAttribute (sTitleCell, "text", 1);
+
+
+//		//////////////////////////////////////////////////////////////////////////
+//		// example spawning new window 
+//		// Create a Window
+//		Gtk.Window window = new Gtk.Window ("TreeView Example");
+//		window.SetSizeRequest (500,200);
+//
+//		// Create our TreeView
+//		Gtk.TreeView tree = new Gtk.TreeView ();
+//
+//		// Add our tree to the window
+//		window.Add (tree);
+//
+//		// Create a column for the artist name
+//		Gtk.TreeViewColumn artistColumn = new Gtk.TreeViewColumn ();
+//		artistColumn.Title = "Artist";
+//
+//		// Create a column for the song title
+//		Gtk.TreeViewColumn songColumn = new Gtk.TreeViewColumn ();
+//		songColumn.Title = "Song Title";
+//
+//		// Add the columns to the TreeView
+//		tree.AppendColumn (artistColumn);
+//		tree.AppendColumn (songColumn);
+//
+//		// Create a model that will hold two strings - Artist Name and Song Title
+//		Gtk.ListStore musicListStore = new Gtk.ListStore (typeof (string), typeof (string));
+//
+//
+//		// Assign the model to the TreeView
+//		tree.Model = musicListStore;
+//
+//		// Add some data to the store
+//		musicListStore.AppendValues ("Garbage", "Dog New Tricks");
+//
+//		// Create the text cell that will display the artist name
+//		Gtk.CellRendererText artistNameCell = new Gtk.CellRendererText ();
+//
+//		// Add the cell to the column
+//		artistColumn.PackStart (artistNameCell, true);
+//
+//		// Do the same for the song title column
+//		Gtk.CellRendererText songTitleCell = new Gtk.CellRendererText ();
+//		songColumn.PackStart (songTitleCell, true);
+//
+//		// Tell the Cell Renderers which items in the model to display
+//		artistColumn.AddAttribute (artistNameCell, "text", 0);
+//		songColumn.AddAttribute (songTitleCell, "text", 1);
+//
+//		// Show the window and everything on it
+//		window.ShowAll ();
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
 		Application.Quit ();
 		a.RetVal = true;
+	}
+
+	[Gtk.TreeNode (ListOnly=true)]
+	public class MyTreeNode : Gtk.TreeNode {
+
+		string song_title;
+
+		public MyTreeNode (string artist, string song_title)
+		{
+			Artist = artist;
+			this.song_title = song_title;
+		}
+
+		[Gtk.TreeNodeValue (Column=0)]
+		public string Artist;
+
+		[Gtk.TreeNodeValue (Column=1)]
+		public string SongTitle {get { return song_title; } }
 	}
 
 	protected void handleConnectionWithServer(object o, bool bIsConnected)
@@ -34,6 +139,7 @@ public partial class MainWindow: Gtk.Window
 				while( (data = dopsHandler.waitForIncomming()).Length > 0 && ((Gtk.Action)o).StockId == "gtk-disconnect") {
 					this.UpdateStatusBarText("Receiving incomming data from DOPs SERVER...");
 					dedAnalyzed dana = chatHandler.parseDEDpacket(data);
+
 					this.UpdateStatusBarText(dana.type);
 				}
 				this.UpdateStatusBarText("STOPPED receiving incomming data from DOPs SERVER - possible ERROR");
--- csharp_projects/cloudchatAdministrator/ccAdmin/gtk-gui/MainWindow.cs
+++ csharp_projects/cloudchatAdministrator/ccAdmin/gtk-gui/MainWindow.cs
@@ -119,7 +119,7 @@ public partial class MainWindow
 		this.notebook1 = new global::Gtk.Notebook ();
 		this.notebook1.CanFocus = true;
 		this.notebook1.Name = "notebook1";
-		this.notebook1.CurrentPage = 1;
+		this.notebook1.CurrentPage = 0;
 		// Container child notebook1.Gtk.Notebook+NotebookChild
 		this.GtkScrolledWindow = new global::Gtk.ScrolledWindow ();
 		this.GtkScrolledWindow.Name = "GtkScrolledWindow";
