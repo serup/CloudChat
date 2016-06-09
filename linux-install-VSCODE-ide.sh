44c25fd3afc3d173a07df4036ae2b4d0ab37fd00
Johnny Serup <serup@hotmail.com>
2016-06-09 22:39:17 +0200

    ISSUE - Add 32bit install version of vscode
    Change-Id: Ib187b3e51e6719d994672b332bea560f9ad5a6d1

--- linux-install-VSCODE-ide.sh
+++ linux-install-VSCODE-ide.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 # Specify IDE version here.  -- NB! MUST RUN as administrator - like this: sudo bash  ./linux-install-VSCODE-ide.sh
 
-VSCODE_PACKAGE=VSCode-linux-x64-stable.zip
-#wget -O "/tmp/$VSCODE_PACKAGE" https://az764295.vo.msecnd.net/stable/def9e32467ad6e4f48787d38caf190acbfee5880/$VSCODE_PACKAGE
-#wget -O "/tmp/$VSCODE_PACKAGE" https://az764295.vo.msecnd.net/stable/809e7b30e928e0c430141b3e6abf1f63aaf55589/$VSCODE_PACKAGE
+
+PLATFORM=$(uname -i)
+if [ "i686" == "$PLATFORM" ]; then
+  VSCODE_PACKAGE=VSCode-linux-ia32-stable.zip
+else
+  VSCODE_PACKAGE=VSCode-linux-x64-stable.zip
+fi
 
 if [ -f "/tmp/$VSCODE_PACKAGE" ]; then
   echo "package exists already - no need to download"
@@ -21,6 +25,19 @@ if [ -f "/tmp/$VSCODE_PACKAGE" ]; then
   sudo rsync -av "$VSCODE_EXTRACTED_FOLDER" /opt/vscode
   sudo cp $VSCODE_EXTRACTED_FOLDER/resources/app/resources/linux/code.png /usr/share/pixmaps/
 
+if [ "i686" == "$PLATFORM" ]; then
+  echo '[Desktop Entry]
+Name=VSCODE IDE
+Type=Application
+Exec=/opt/vscode/VSCode-linux-ia32/bin/code
+Icon=/opt/vscode/VSCode-linux-ia32/resources/app/resources/linux/code.png
+Terminal=false
+Comment=Integrated Development Environment
+NoDisplay=false
+Categories=Development;IDE;
+Name[en]=VSCODE IDEA
+' > vscode.desktop
+else
   echo '[Desktop Entry]
 Name=VSCODE IDE
 Type=Application
@@ -32,12 +49,16 @@ NoDisplay=false
 Categories=Development;IDE;
 Name[en]=VSCODE IDEA
 ' > vscode.desktop
+fi
 
   sudo desktop-file-install vscode.desktop
   sudo rm -f vscode.desktop
   sudo rm -f /usr/local/bin/code
-  sudo ln -s /opt/vscode/VSCode-linux-x64/bin/code /usr/local/bin/code
-
+  if [ "i686" == "$PLATFORM" ]; then
+    sudo ln -s /opt/vscode/VSCode-linux-ia32/bin/code /usr/local/bin/code
+  else
+    sudo ln -s /opt/vscode/VSCode-linux-x64/bin/code /usr/local/bin/code
+  fi
 else
   echo "error - installation of vscode ide failed"
   echo "You can use Ubuntu Make to download and install Visual Studio Code:"
