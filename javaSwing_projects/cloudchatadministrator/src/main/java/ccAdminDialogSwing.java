933c2745870428adaa7b13037d326c94a7999b77
johnny serup <serup@hotmail.com>
2016-06-10 14:37:41 +0200

    PROJECT - new cloudchatadministrator in Java with swing ui
    Change-Id: If8204868aba7f00807de14259564adc8cd464959

--- javaSwing_projects/cloudchatadministrator/src/main/java/ccAdminDialogSwing.java
+++ javaSwing_projects/cloudchatadministrator/src/main/java/ccAdminDialogSwing.java
@@ -5,6 +5,10 @@ public class ccAdminDialogSwing extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
+    private JTabbedPane tabbedPane;
+    private JList listManagers;
+    private JList listCustomers;
+    private JButton buttonConnect;
 
     public ccAdminDialogSwing() {
         setContentPane(contentPane);
@@ -37,6 +41,12 @@ public class ccAdminDialogSwing extends JDialog {
                 onCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        listManagers.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+            }
+        });
     }
 
     private void onOK() {
@@ -54,5 +64,9 @@ public class ccAdminDialogSwing extends JDialog {
         dialog.pack();
         dialog.setVisible(true);
         System.exit(0);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
