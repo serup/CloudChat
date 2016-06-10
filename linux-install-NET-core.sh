88379c813788cba98ab991da011d9af19adeae2a
johnny serup <serup@hotmail.com>
2016-06-10 10:40:23 +0200

    REFACTOR - issue though regarding .NET C# plugin for vscode - 16.04 lts 64bit - not working yet
    Change-Id: I95ca37f364dc27e7a0fb343ce371a1546bccc610

--- linux-install-NET-core.sh
+++ linux-install-NET-core.sh
@@ -1,17 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env bash
 echo "************************************************"
 echo " INSTALL .NET CORE "
 echo "************************************************"
+
 PLATFORM=$(uname -i)
 if [ "i686" == "$PLATFORM" ]; then
   echo "SORRY NO 32bit version for now"
 else
-  sudo apt-get install libicu-dev
   sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list' 
   sudo sh -c 'echo "deb http://security.ubuntu.com/ubuntu trusty-security main" >> /etc/apt/sources.list.d/dotnetdev.list' 
   sudo apt-get update
+  wget -O "libicu52_52.1-3ubuntu0.4_amd64.deb" http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu52_52.1-3ubuntu0.4_amd64.deb
   sudo dpkg -i libicu52_52.1-3ubuntu0.4_amd64.deb 
   sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+  sudo apt-get update
   sudo apt-get install dotnet-dev-1.0.0-preview1-002702
   echo "************************************************"
   echo " - lets test it: "
