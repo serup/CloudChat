5d0c389fb6985140271ab4d51f6bcab7d6bde8d1
johnny serup <serup@hotmail.com>
2016-06-13 10:35:01 +0200

    BUGFIX - possible auto dispose of Task object, caused exception
    Change-Id: I6c87d50e98b75d23597516b7281627388681bb46

--- csharp_projects/cloudchatAdministrator/ccAdmin/WebSocket/Client.cs
+++ csharp_projects/cloudchatAdministrator/ccAdmin/WebSocket/Client.cs
@@ -17,7 +17,6 @@ namespace WebSocketClient
 		private const int BufferAmplifier = 20;
 		private static int maxAmountOfBytesToRead = BufferSize;
 		private byte[] bufferForRead = new byte[maxAmountOfBytesToRead];
-		private ArraySegment<byte> linkToBufferForRead;
 		private byte[] receivedBuffer = null;
 		EventWaitHandle signalDataHasArrived  = new AutoResetEvent (false); // is signaled when data is ready in receivedBuffer;
 		bool signalToStopReceiving = false; // continue reading until notified to stop
@@ -25,8 +24,6 @@ namespace WebSocketClient
 		public bool Connect(string uri) {
 			bool bResult = false;
 			try {
-				linkToBufferForRead = new ArraySegment<byte>(bufferForRead);
-
 				ConnectAsync(new Uri(uri), CancellationToken.None).Wait();
 				if(State == WebSocketState.Open) {
 					setupReceiveTask();
@@ -48,47 +45,48 @@ namespace WebSocketClient
 		{
 			bool bResult = true;
 			try {
-				new Task(async () => {
-					Console.WriteLine ("Receiving Task has started");
-					var temporaryBuffer = new byte[BufferSize];
-					var buffer = new byte[BufferSize*BufferAmplifier];
-					var offset = 0;
-					WebSocketReceiveResult response;
-					do {
-						do 
-						{
-							response = await ReceiveAsync(new ArraySegment<byte>(temporaryBuffer), CancellationToken.None);
-							temporaryBuffer.CopyTo(buffer, offset);
-							offset += response.Count;
-							temporaryBuffer = new byte[BufferSize];
-						} while (!response.EndOfMessage);
-						if (response.MessageType != WebSocketMessageType.Close )
-						{
-							if (bufferForRead.Length < response.Count) {
-								Console.WriteLine ("WARNING - incomming data is larger than internal buffer - trying to receive and merge rest !!!");
-								receivedBuffer = null;
-								/*  DOES NOT WORK - MISSING BYTES IN BETWEEN - FIND A BETTER WAY
-								int restBytes = result.Count - buffer.Length;
-								byte[] restBuffer = new byte[restBytes];
-								receivedBuffer = new byte[result.Count]; // make room for ALL data
-								Buffer.BlockCopy (buffer, 0, receivedBuffer, 0, buffer.Length); // copy first part
-								// try to receive waiting bytes
-								result = await webSocket.ReceiveAsync(new ArraySegment<byte>(restBuffer), CancellationToken.None);
-								Buffer.BlockCopy (restBuffer, 0, receivedBuffer, buffer.Length-1, restBuffer.Length); // copy rest part
-								*/
-								signalDataHasArrived.Set (); // signal that data has been received - this will wake up WaitForData() - which will then return it to user
-							}
-							else {
-								receivedBuffer = new byte[response.Count];
-								Buffer.BlockCopy (buffer, 0, receivedBuffer, 0, response.Count);
-								buffer = new byte[BufferSize*BufferAmplifier];
-								offset = 0;
-								signalDataHasArrived.Set (); // signal that data has been received - this will wake up WaitForData() - which will then return it to user
-							}
-						}
-					}while(signalToStopReceiving==false && State == WebSocketState.Open);
-					Console.WriteLine ("Receiving Task has stopped");
-				}).Start();
+//				new Task(async () => {
+//					Console.WriteLine ("Receiving Task has started");
+//					var temporaryBuffer = new byte[BufferSize];
+//					var buffer = new byte[BufferSize*BufferAmplifier];
+//					var offset = 0;
+//					WebSocketReceiveResult response;
+//					do {
+//						do 
+//						{
+//							response = await ReceiveAsync(new ArraySegment<byte>(temporaryBuffer), CancellationToken.None);
+//							temporaryBuffer.CopyTo(buffer, offset);
+//							offset += response.Count;
+//							temporaryBuffer = new byte[BufferSize];
+//						} while (!response.EndOfMessage);
+//						if (response.MessageType != WebSocketMessageType.Close )
+//						{
+//							if (bufferForRead.Length < response.Count) {
+//								Console.WriteLine ("WARNING - incomming data is larger than internal buffer - trying to receive and merge rest !!!");
+//								receivedBuffer = null;
+//								/*  DOES NOT WORK - MISSING BYTES IN BETWEEN - FIND A BETTER WAY
+//								int restBytes = result.Count - buffer.Length;
+//								byte[] restBuffer = new byte[restBytes];
+//								receivedBuffer = new byte[result.Count]; // make room for ALL data
+//								Buffer.BlockCopy (buffer, 0, receivedBuffer, 0, buffer.Length); // copy first part
+//								// try to receive waiting bytes
+//								result = await webSocket.ReceiveAsync(new ArraySegment<byte>(restBuffer), CancellationToken.None);
+//								Buffer.BlockCopy (restBuffer, 0, receivedBuffer, buffer.Length-1, restBuffer.Length); // copy rest part
+//								*/
+//								signalDataHasArrived.Set (); // signal that data has been received - this will wake up WaitForData() - which will then return it to user
+//							}
+//							else {
+//								receivedBuffer = new byte[response.Count];
+//								Buffer.BlockCopy (buffer, 0, receivedBuffer, 0, response.Count);
+//								buffer = new byte[BufferSize*BufferAmplifier];
+//								offset = 0;
+//								signalDataHasArrived.Set (); // signal that data has been received - this will wake up WaitForData() - which will then return it to user
+//							}
+//						}
+//					}while(signalToStopReceiving==false && State == WebSocketState.Open);
+//					Console.WriteLine ("Receiving Task has stopped");
+//				}).Start();
+				_Receive().Start();
 			}
 			catch(Exception e) {
 				//Console.WriteLine(e.Message.ToString());
@@ -96,6 +94,40 @@ namespace WebSocketClient
 				bResult = false;
 			}
 			return bResult;
+		}
+
+		private async Task _Receive()
+		{
+			Console.WriteLine ("Receiving Task has started");
+			var temporaryBuffer = new byte[BufferSize];
+			var buffer = new byte[BufferSize*BufferAmplifier];
+			var offset = 0;
+			WebSocketReceiveResult response;
+			do {
+				do 
+				{
+					response = await ReceiveAsync(new ArraySegment<byte>(temporaryBuffer), CancellationToken.None);
+					temporaryBuffer.CopyTo(buffer, offset);
+					offset += response.Count;
+					temporaryBuffer = new byte[BufferSize];
+				} while (!response.EndOfMessage);
+				if (response.MessageType != WebSocketMessageType.Close )
+				{
+					if (bufferForRead.Length < response.Count) {
+						Console.WriteLine ("WARNING - incomming data is larger than internal buffer - trying to receive and merge rest !!!");
+						receivedBuffer = null;
+						signalDataHasArrived.Set (); // signal that data has been received - this will wake up WaitForData() - which will then return it to user
+					}
+					else {
+						receivedBuffer = new byte[response.Count];
+						Buffer.BlockCopy (buffer, 0, receivedBuffer, 0, response.Count);
+						buffer = new byte[BufferSize*BufferAmplifier];
+						offset = 0;
+						signalDataHasArrived.Set (); // signal that data has been received - this will wake up WaitForData() - which will then return it to user
+					}
+				}
+			}while(signalToStopReceiving==false && State == WebSocketState.Open);
+			Console.WriteLine ("Receiving Task has stopped");
 		}
 
 		public async Task SendBLOB(byte[] buffer)
