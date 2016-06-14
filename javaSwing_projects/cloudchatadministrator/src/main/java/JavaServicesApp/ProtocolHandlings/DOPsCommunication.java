576d8d4e052cbce516710e49e75655c5bd1db8d2
johnny serup <serup@hotmail.com>
2016-06-14 14:37:02 +0200

    PROJECT[cloudchatAdministrator; Java spring Swing version] - startHandlingIncomingDataThread
    Change-Id: Ieebcc8f8800a214d512fa5d2604dc6cb89193ce9

--- javaSwing_projects/cloudchatadministrator/src/main/java/JavaServicesApp/ProtocolHandlings/DOPsCommunication.java
+++ javaSwing_projects/cloudchatadministrator/src/main/java/JavaServicesApp/ProtocolHandlings/DOPsCommunication.java
@@ -11,9 +11,10 @@ import java.nio.ByteBuffer;
  * Created by serup on 09-05-16.
  */
 public class DOPsCommunication {
-    private JavaWebSocketClientEndpoint clientEndpoint=null;
+    private JavaWebSocketClientEndpoint clientEndpoint = null;
+    private Thread handlingIncomingDataThread;
 
-    public boolean connectToDOPs(String uniqueId, String username, String password){
+    public boolean connectToDOPs(String uniqueId, String username, String password) {
         boolean bResult;
         clientEndpoint = new JavaWebSocketClientEndpoint();
         clientEndpoint.connectToServer("ws://backend.scanva.com:7777");
@@ -26,26 +27,43 @@ public class DOPsCommunication {
     }
 
     public boolean setupCommunication() throws Exception {
-        boolean bResult=false;
-        if(clientEndpoint!=null) {
+        boolean bResult = false;
+        if (clientEndpoint != null) {
             System.out.println("Setting up thread for handling incomming DOPs data packages");
+            bResult = startHandlingIncomingDataThread();
 
-
-            bResult=true;
-            throw new Exception("not fully implemented ");
-        }
-        else
+        } else
             System.out.println("ERROR: NOT possible to setup thread for handling incomming DOPs data packages");
 
         return bResult;
     }
 
+    private boolean startHandlingIncomingDataThread() throws InterruptedException {
+        final boolean[] bResult = {false};
+        handlingIncomingDataThread = new Thread() {
+            public void run() {
+                try {
+                    bResult[0] = true;
+
+                    while (clientEndpoint.receiveFromServer() != null);
+
+                    throw new Exception("handlingIncomingDataThread - stopped");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    bResult[0] = false;
+                }
+            }
+        };
+        handlingIncomingDataThread.start();
+        Thread.sleep(100); // wait a bit for thread to start
+        return bResult[0];
+    }
+
     public boolean disconnectFromDOPs() throws IOException {
-        boolean bResult=false;
-        if(clientEndpoint!=null)
-        {
+        boolean bResult = false;
+        if (clientEndpoint != null) {
             clientEndpoint.clientSession.close();
-            bResult=true;
+            bResult = true;
         }
         return bResult;
     }
@@ -57,97 +75,82 @@ public class DOPsCommunication {
     }
 
 
-    private static String decodeLoginResponse(byte[] receivedData)
-    {
-        String Result="dops.decode.status.error";
-        boolean bDecoded=false;
-        String strMethod="";
-        String strProtocolTypeID="";
-        String strFunctionName="";
-        String strStatus="";
-        short uTrans_id=0;
+    private static String decodeLoginResponse(byte[] receivedData) {
+        String Result = "dops.decode.status.error";
+        boolean bDecoded = false;
+        String strMethod = "";
+        String strProtocolTypeID = "";
+        String strFunctionName = "";
+        String strStatus = "";
+        short uTrans_id = 0;
 
         // decode data ...
         DEDDecoder DED2 = new DEDDecoder();
-        DED2.PUT_DATA_IN_DECODER( receivedData, receivedData.length);
-        if( DED2.GET_STRUCT_START( "WSResponse" )==1 &&
-                (strMethod   = DED2.GET_METHOD ( "Method" )).length()>0 &&
-                (uTrans_id     = DED2.GET_USHORT ( "TransID")) !=-1 &&
-                (strProtocolTypeID  = DED2.GET_STDSTRING ( "protocolTypeID")).length()>0 )
-        {
-            if(strMethod.equals("1_1_6_LoginProfile"))
-            {
-                String strDestination="";
-                String strSource="";
+        DED2.PUT_DATA_IN_DECODER(receivedData, receivedData.length);
+        if (DED2.GET_STRUCT_START("WSResponse") == 1 &&
+                (strMethod = DED2.GET_METHOD("Method")).length() > 0 &&
+                (uTrans_id = DED2.GET_USHORT("TransID")) != -1 &&
+                (strProtocolTypeID = DED2.GET_STDSTRING("protocolTypeID")).length() > 0) {
+            if (strMethod.equals("1_1_6_LoginProfile")) {
+                String strDestination = "";
+                String strSource = "";
                 // login response from DFD was received, now decode response
-                if ( (strDestination = DED2.GET_STDSTRING ( "dest")).length()>0 &&
-                        (strSource      = DED2.GET_STDSTRING ( "src")).length()>0 &&
-                        (strStatus      = DED2.GET_STDSTRING ( "status")).length()>0 &&
-                        DED2.GET_STRUCT_END( "WSResponse" )==1)
-                {
+                if ((strDestination = DED2.GET_STDSTRING("dest")).length() > 0 &&
+                        (strSource = DED2.GET_STDSTRING("src")).length() > 0 &&
+                        (strStatus = DED2.GET_STDSTRING("status")).length() > 0 &&
+                        DED2.GET_STRUCT_END("WSResponse") == 1) {
                     // response from login to profile in DFD was received - now validate status
-                    if(strStatus.equals("ACCEPTED")) {
-                        bDecoded=true;
-                        System.out.println("1_1_6_LoginProfile response packet decoded; src: "+strSource+" ; dest: "+strDestination+" ; Status: ACCEPTED");
-                    }
-                    else
-                    {
-                        bDecoded=true; // set to true since login failure is actually a correct response, since client is not registered
-                        System.out.println("1_1_6_LoginProfile response packet decoded; however login failed ;  Status: "+strStatus);
+                    if (strStatus.equals("ACCEPTED")) {
+                        bDecoded = true;
+                        System.out.println("1_1_6_LoginProfile response packet decoded; src: " + strSource + " ; dest: " + strDestination + " ; Status: ACCEPTED");
+                    } else {
+                        bDecoded = true; // set to true since login failure is actually a correct response, since client is not registered
+                        System.out.println("1_1_6_LoginProfile response packet decoded; however login failed ;  Status: " + strStatus);
                     }
                 }
-            }
-            else
-            { // When DFD is offline, then this type of packet is received
+            } else { // When DFD is offline, then this type of packet is received
                 if (
-                        (strFunctionName    = DED2.GET_STDSTRING ( "functionName")).length()>0 &&
-                                (strStatus  = DED2.GET_STDSTRING ( "status")).length()>0 &&
-                                DED2.GET_STRUCT_END( "WSResponse" )==1)
-                {
-                    bDecoded=true;
+                        (strFunctionName = DED2.GET_STDSTRING("functionName")).length() > 0 &&
+                                (strStatus = DED2.GET_STDSTRING("status")).length() > 0 &&
+                                DED2.GET_STRUCT_END("WSResponse") == 1) {
+                    bDecoded = true;
                     System.out.println("DED packet decoded - now validate");
 
-                    if(!strMethod.equals("JavaConnect")) bDecoded=false;
-                }
-                else
-                {
+                    if (!strMethod.equals("JavaConnect")) bDecoded = false;
+                } else {
                     // unknown method
-                    System.out.println("DED packet could NOT be decoded - unknown Method: "+strMethod);
+                    System.out.println("DED packet could NOT be decoded - unknown Method: " + strMethod);
                 }
             }
-        }
-        else
-        {
-            bDecoded=false;
+        } else {
+            bDecoded = false;
         }
 
-        if(bDecoded)
-            Result="dops.connected.status.ok";
+        if (bDecoded)
+            Result = "dops.connected.status.ok";
         return Result;
     }
 
-    public static dedAnalyzed decodeIncomingDED(byte[] DED) throws  Exception
-    {
+    public static dedAnalyzed decodeIncomingDED(byte[] DED) throws Exception {
         dedAnalyzed dana = new dedAnalyzed();
 
-        if(DED == null) {
+        if (DED == null) {
             dana.type = "<unknown>";
             dana.bDecoded = false;
         } else {
             // decode data ...
             DEDDecoder DED2 = new DEDDecoder();
-            DED2.PUT_DATA_IN_DECODER( DED, DED.length);
-            if((DED2.GET_STRUCT_START("CloudManagerRequest") == 1)) {
+            DED2.PUT_DATA_IN_DECODER(DED, DED.length);
+            if ((DED2.GET_STRUCT_START("CloudManagerRequest") == 1)) {
                 String method = DED2.GET_METHOD("Method");
-                switch (method)
-                {
+                switch (method) {
                     case "JSCForwardInfo":
                         ForwardInfoRequestObj fio = new ForwardInfoRequestObj();
-                        if((fio.transactionsID  = DED2.GET_USHORT("TransID")) != -1 &&
+                        if ((fio.transactionsID = DED2.GET_USHORT("TransID")) != -1 &&
                                 (fio.protocolTypeID = DED2.GET_STDSTRING("protocolTypeID")).contains("DED1.00.00") &&
-                                (fio.dest           = DED2.GET_STDSTRING("dest")).length() > 0 &&
-                                (fio.src            = DED2.GET_STDSTRING("src")).length() > 0 &&
-                                (fio.srcAlias       = DED2.GET_STDSTRING("srcAlias")).length() > 0 &&
+                                (fio.dest = DED2.GET_STDSTRING("dest")).length() > 0 &&
+                                (fio.src = DED2.GET_STDSTRING("src")).length() > 0 &&
+                                (fio.srcAlias = DED2.GET_STDSTRING("srcAlias")).length() > 0 &&
                                 (DED2.GET_STRUCT_END("CloudManagerRequest")) == 1) {
                             dana.bDecoded = true;
                             dana.type = "ForwardInfoRequest";
@@ -157,13 +160,13 @@ public class DOPsCommunication {
                         break;
                     case "JSCChatInfo":
                         ChatInfoObj cio = new ChatInfoObj();
-                        if((cio.transactionsID    = DED2.GET_USHORT("TransID")) != -1 &&
-                                (cio.protocolTypeID   = DED2.GET_STDSTRING("protocolTypeID")).contains("DED1.00.00") &&
-                                (cio.dest             = DED2.GET_STDSTRING("dest")).length() > 0 &&
-                                (cio.src              = DED2.GET_STDSTRING("src")).length() > 0 &&
-                                (cio.srcAlias         = DED2.GET_STDSTRING("srcAlias")).length() > 0 &&
+                        if ((cio.transactionsID = DED2.GET_USHORT("TransID")) != -1 &&
+                                (cio.protocolTypeID = DED2.GET_STDSTRING("protocolTypeID")).contains("DED1.00.00") &&
+                                (cio.dest = DED2.GET_STDSTRING("dest")).length() > 0 &&
+                                (cio.src = DED2.GET_STDSTRING("src")).length() > 0 &&
+                                (cio.srcAlias = DED2.GET_STDSTRING("srcAlias")).length() > 0 &&
                                 (cio.srcHomepageAlias = DED2.GET_STDSTRING("srcHomepageAlias")).length() > 0 &&
-                                (cio.lastEntryTime    = DED2.GET_STDSTRING("lastEntryTime")).length() > 0 &&
+                                (cio.lastEntryTime = DED2.GET_STDSTRING("lastEntryTime")).length() > 0 &&
                                 (DED2.GET_STRUCT_END("ClientChatRequest")) == 1) {
                             dana.bDecoded = true;
                             dana.type = "ChatInfo";
@@ -181,17 +184,17 @@ public class DOPsCommunication {
         }
         return dana;
     }
-    private static ByteBuffer createDEDforDOPSJavaConnect(short trans_id, String uniqueId, String username, String password)
-    {
+
+    private static ByteBuffer createDEDforDOPSJavaConnect(short trans_id, String uniqueId, String username, String password) {
         DEDEncoder DED = new DEDEncoder();
-        DED.PUT_STRUCT_START ( "WSRequest" );
-        DED.PUT_METHOD   ( "Method",  "JavaConnect" );
-        DED.PUT_USHORT   ( "TransID",  trans_id);
-        DED.PUT_STDSTRING( "protocolTypeID", "DED1.00.00");
-        DED.PUT_STDSTRING( "functionName", uniqueId );
-        DED.PUT_STDSTRING( "username", username );
-        DED.PUT_STDSTRING( "password", password );
-        DED.PUT_STRUCT_END( "WSRequest" );
+        DED.PUT_STRUCT_START("WSRequest");
+        DED.PUT_METHOD("Method", "JavaConnect");
+        DED.PUT_USHORT("TransID", trans_id);
+        DED.PUT_STDSTRING("protocolTypeID", "DED1.00.00");
+        DED.PUT_STDSTRING("functionName", uniqueId);
+        DED.PUT_STDSTRING("username", username);
+        DED.PUT_STDSTRING("password", password);
+        DED.PUT_STRUCT_END("WSRequest");
 
         ByteBuffer data = DED.GET_ENCODED_BYTEBUFFER_DATA();
         return data;
