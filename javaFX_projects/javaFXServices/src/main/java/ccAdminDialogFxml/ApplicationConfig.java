eaf8b3184f2aaea075a3a5e91cc0ec92a35b4346
johnny serup <serup@hotmail.com>
2016-06-21 10:23:35 +0200

    CLEAN
    Change-Id: Ie8daf5222aa6fd8c6978bc981eb9a577d645fe2e

--- javaFX_projects/javaFXServices/src/main/java/ccAdminDialogFxml/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package ccAdminDialogFxml;
-
-public class ApplicationConfig {
-
-	public static final String FXML_URL = "/scenebuilderFiles/ccAdminApp_css.fxml";
-	public static final String CSS_URL = "/scenebuilderFiles/ccAdminApp.css";
-	public static final String RESOURCE_BUNDLE_NAME = "ccAdmin";
-
-}
