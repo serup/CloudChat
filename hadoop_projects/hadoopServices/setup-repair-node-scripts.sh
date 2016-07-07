7e1e3fc20ce29f6801ed95758db8c406d0541632
johnny serup <serup@hotmail.com>
2016-07-07 16:24:24 +0200

    ISSUE - manually running scripts elements is still needed
    Change-Id: Ibb2379e4e6afbdd8421751f073e486a46ef9e843

--- hadoop_projects/hadoopServices/setup-repair-node-scripts.sh
+++ hadoop_projects/hadoopServices/setup-repair-node-scripts.sh
@@ -2,3 +2,11 @@
 sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp bootstrap-server.sh vagrant@one.cluster:/home/vagrant/.
 sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp bootstrap-node.sh vagrant@two.cluster:/home/vagrant/.
 sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp bootstrap-node.sh vagrant@three.cluster:/home/vagrant/.
+
+echo 'sudo -su hdfs
+hadoop dfsadmin -safemode leave
+hadoop fs -rmr -skipTrash /tmp/input/findprofile
+hadoop fsck -files delete /' > tmpDeleteCorruptFiles.sh
+
+sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp tmpDeleteCorruptFiles.sh vagrant@one.cluster:/home/vagrant/.
+
