5086ef37c9eb2e5bc61e5d746d50eca5d18141db
johnny serup <serup@hotmail.com>
2016-07-12 14:26:47 +0200

    ISSUE - use smaller profile default image and mogrify to smaller thumbnails
    Change-Id: Id73af0747199dbed977f4e382444a0d46de185d7

--- replication.sh
+++ replication.sh
@@ -1,9 +1,8 @@
 #!/bin/bash 
-# First resize to a maximum of 200x200 pixels
-mogrify -resize 200x200 *.jpg
+# First resize to a maximum of 80x80 pixels
+mogrify -resize 80x80 *.jpg
 # Then transfer files to frontend server
-#did not work -- sshpass -p "<password>" scp -rp /var/www/img/ cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/.
-#sshpass -p "<password>" scp -oStrictHostKeyChecking=no -rp /var/www/img/* cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/img/.
+#sshpass -p "<password>" scp -oStrictHostKeyChecking=no -rp /var/www/img/ cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/.
 sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/html/CloudChatManager/. 
 #sleep 1000
 # Cleanup
