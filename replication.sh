8a8d2b5a91b79ea70e9e10eba207a371ae328a08
johnny serup <serup@hotmail.com>
2016-07-13 10:26:30 +0200

    ISSUE - mogrify still not working
    Change-Id: I80470c68bc853348fa77c65b6b95896873f3738d

--- replication.sh
+++ replication.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 # First resize to a maximum of 80x80 pixels
-mogrify -resize 80x80 '*.jpg'
-mogrify -resize 80x80 '*.png'
+mogrify -resize 80x80 '/var/www/img/*.jpg'
+mogrify -resize 80x80 '/var/www/img/*.png'
 # Then transfer files to frontend server
 #sshpass -p "<password>" scp -oStrictHostKeyChecking=no -rp /var/www/img/ cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/.
 sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/html/CloudChatManager/. 
