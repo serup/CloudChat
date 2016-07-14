125cc1c6b07ad6ba3f4906af006925e32631e59b
johnny serup <serup@hotmail.com>
2016-07-14 14:55:41 +0200

    ISSUE - replication
    Change-Id: Ie9e4643d06396bf9714f22e5390cff93829a5e2b

--- replication.sh
+++ replication.sh
@@ -1,17 +1,13 @@
 #!/bin/bash 
-mkdir .thumbs
+mkdir -p /var/www/.thumbs
+cd /var/www/img
 # resize to a maximum of 200x200 pixels in .thumbs subdir
-gm mogrify -output-directory .thumbs -resize 150x170 "/var/www/img/*"
-
-# Final move resulting image back - oups that will cause deamon to run again
-#mv .thumbs/* /var/www/img/.  
-
+gm mogrify -output-directory /var/www/.thumbs -resize 150x170 "*"
 # Then transfer files to frontend server
-#sshpass -p "<password>" scp -oStrictHostKeyChecking=no -rp /var/www/img/ cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/.
-#sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/html/CloudChatManager/. 
-sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp .thumbs/* vagrant@cloudchatmanager.com:/var/www/html/CloudChatManager/img/ 
+#sshpass -p "<password>" scp -oStrictHostKeyChecking=no -rp /var/www/.thumbs/* cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/img/
+sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/.thumbs/* vagrant@cloudchatmanager.com:/var/www/html/CloudChatManager/img/
 #sleep 1000
 # Cleanup
-rm -rf .thumbs 
-rm -f /var/www/img/*
+#rm -rf /var/www/.thumbs 
+#rm -f /var/www/img/*
 exec "$@"
