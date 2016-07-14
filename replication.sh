4d0ed5566d48f3a34c3303d465333b7b2fdb4ce0
johnny serup <serup@hotmail.com>
2016-07-14 14:24:00 +0200

    ISSUE - use gm mogrify instead
    Change-Id: I8434348c2163ca76a6b763608928cabfbb321648

--- replication.sh
+++ replication.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 mkdir .thumbs
 # resize to a maximum of 200x200 pixels in .thumbs subdir
-gm mogrify -output-directory .thumbs -resize 150x170 '/var/www/img/*.png'
+gm mogrify -output-directory .thumbs -resize 150x170 "/var/www/img/*"
 # Final move resulting image back
-mv .thumbs/*.png /var/www/img/.  
+mv .thumbs/* /var/www/img/.  
 # Then transfer files to frontend server
 #sshpass -p "<password>" scp -oStrictHostKeyChecking=no -rp /var/www/img/ cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/.
 sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/html/CloudChatManager/. 
