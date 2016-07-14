7daef6bced89179cea659c4b33716d2e8bc83f71
johnny serup <serup@hotmail.com>
2016-07-14 09:05:38 +0200

    BUGFIX - replication.sh mogrify must use tmp area to perform resize
    Change-Id: I57b063a9563f56684158f9d4e846d780b074fb6a

--- replication.sh
+++ replication.sh
@@ -1,10 +1,17 @@
 #!/bin/bash 
-# First resize to a maximum of 80x80 pixels
-mogrify -resize 80x80 '/var/www/img/*.jpg'
+# First move image to new tmp area
+mkdir -p /var/tmp/mogrifyTemp
+cp /var/www/img/*.jpg /var/tmp/mogrifyTemp/.
+# Then resize to a maximum of 200x200 pixels
+mogrify -resize 150x170 '/var/tmp/mogrifyTemp/*.jpg'
+# Final move resulting image back
+cp /var/tmp/mogrifyTemp/*.jpg /var/www/img/.  
+# cleanup tmp area
+rm -rf /var/tmp/mogrifyTemp
 # Then transfer files to frontend server
 #sshpass -p "<password>" scp -oStrictHostKeyChecking=no -rp /var/www/img/ cloud1scanvacom@77.75.165.130:/var/www/html/CloudChatManager/.
 sshpass -p "vagrant" scp -oStrictHostKeyChecking=no -rp /var/www/img/ vagrant@cloudchatmanager.com:/var/www/html/CloudChatManager/. 
 #sleep 1000
 # Cleanup
-#rm -f /var/www/img/*
+rm -f /var/www/img/*
 exec "$@"
