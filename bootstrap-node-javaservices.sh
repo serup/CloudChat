ad328d2569807aa0070e27c6ed2dd6a9f86b86f1
johnny serup <serup@hotmail.com>
2016-08-12 12:25:17 +0200

    ISSUE - missing library
    Change-Id: Ia4668a60344424e938cd3ee80e036bd1ca2dc974

--- bootstrap-node-javaservices.sh
+++ bootstrap-node-javaservices.sh
@@ -55,6 +55,7 @@ else
 
     echo "install libraries so X11 forwarding will work for javaFX applications"
     sudo apt-get install libgtk2.0-0 libgdk-pixbuf2.0-0 libfontconfig1 libxrender1 libx11-6 libglib2.0-0  libxft2 libfreetype6 libc6 zlib1g libpng12-0 libstdc++6-4.8-dbg-arm64-cross libgcc1 
+    sudo apt-get install libcanberra-gtk3-module
 
     echo "Fetch latest version of CloudChat"
     if [ -d "CloudChat" ]; then
