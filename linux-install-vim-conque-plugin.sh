b9c178be447c159a54b2da98af88121870a0e1b8
johnny serup <serup@hotmail.com>
2016-08-22 10:20:25 +0200

    ADD - Terminal inside VIM split; using :ConqueTerm bash <CR>
    Change-Id: I30c6e43dda9fd0eef5c9c0ad2420ea0adfb3f060

---
+++ linux-install-vim-conque-plugin.sh
@@ -0,0 +1,14 @@
+#!/usr/bin bash
+# 
+# Installation
+#
+# Download the latest vimball from http://conque.googlecode.com
+#
+# Open the .vba or .vmb file with sudo Vim and run the following commands: :so % :q That's it! The :ConqueTerm command will be available the next time you start Vim. You can delete the .vba file when you've verified Conque was successfully installed.
+#
+
+echo "fetch the conque plugin for vim - inside terminal for wim windows"
+#sudo wget -O "conque_2.3.tar.gz" https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/conque/conque_2.3.tar.gz
+sudo wget -O "conque_2.3.vmb" https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/conque/conque_2.3.vmb
+#vim conque_2.3.vmb :so %
+
