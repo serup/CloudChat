8e8304dec272105367b397d8d5108dd76c3651c0
Johnny Serup <serup@hotmail.com>
2016-09-03 10:55:08 +0200

    ADD - tmux resurect and tmux plugin manager
    Change-Id: I299739000f662e06ccc8402bf61a71ca9dbdbbab

--- linux-install-tmux-resurect.sh
+++ linux-install-tmux-resurect.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "fetch tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "fetch tmux resurect plugin"
 git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux
 echo "add to .tmux.conf file"
