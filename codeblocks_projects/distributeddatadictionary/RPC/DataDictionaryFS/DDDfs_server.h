80376963df9bcc079fc46fcf7ce1b23881275ecd
johnny serup <serup@hotmail.com>
2016-10-05 12:22:46 +0200

    [RPC] - dddfsServer.cpp is Main file for handling requests
    Change-Id: I679197d2cfe046764db771a5a4a8f1250e961495

--- codeblocks_projects/distributeddatadictionary/RPC/DataDictionaryFS/DDDfs_server.h
+++ codeblocks_projects/distributeddatadictionary/RPC/DataDictionaryFS/DDDfs_server.h
@@ -1,11 +1,14 @@
 #ifndef DDDFS_SERVER_H
 #define DDDFS_SERVER_H
 
+#include "DDDfs.h"
+
 namespace DDDfsRPC {
 
 	class dddfsServer 
 	{
-
+		public:
+		DEDBlock * handleRequest(struct svc_req *rqstp);
 
 	};
 
---
+++ codeblocks_projects/distributeddatadictionary/RPC/DataDictionaryFS/dddfsServer.cpp
@@ -0,0 +1,7 @@
+#include "DDDfs_server.h"
+using namespace DDDfsRPC;
+
+DEDBlock * dddfsServer::handleRequest(struct svc_req *rqstp)  
+{
+	printf("Hello World\n");
+}
