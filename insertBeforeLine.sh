69efb670f7218b0328d835de1823d03f1b198533
johnny serup <serup@hotmail.com>
2016-10-07 13:47:55 +0200

    [RPC] - add DED TODO in DDDfs_client
    Change-Id: Ic3c75a4f31269df6f820261e43569b9de2dbb824

--- insertBeforeLine.sh
+++ insertBeforeLine.sh
@@ -8,4 +8,40 @@ do
      	tmp1="sed -n 'H;\${x;s/^\n//;s/c64${c}.vm.network .*\n/c64${c}.vm.provider :virtualbox do |vb| vb.name=\"c64${c}\" end\n    &/;p;}' newVagrantfile$((i-1)) > newVagrantfile$((i))";
 	echo "$tmp1" >> tmpsubst.sh
 done
-	
+
+# info - example :
+# 
+# example file test.txt :
+# 
+# hello line 1
+# hello line 2
+# hello line 3
+# 
+# script:
+# 
+# sed -n 'H;${x;s/^\n//;s/hello line 2/hello new line\n&/;p;}' test.txt > test.txt.2
+# 
+# output file test.txt.2
+# 
+# hello line 1
+# hello new line
+# hello line 2
+# hello line 3
+# 
+# NB! notice that the sed has as beginning a substitution of a newline to no space - this is necessary otherwise resulting file will have one empty line in the beginning
+# 
+# The script finds the line containing "hello line 2", it then inserts a new line above -- "hello new line"
+# 
+# explanation of sed commands:
+# 
+# sed -n:
+# suppress automatic printing of pattern space
+# 
+# H;${x;s/test/next/;p}
+# 
+# /<pattern>/  search for a <pattern>
+# ${}  do this 'block' of code
+# H    put the pattern match in the hold space
+# s/   substitute test for next everywhere in the space
+# x    swap the hold with the pattern space
+# p    Print the current pattern hold space. 
