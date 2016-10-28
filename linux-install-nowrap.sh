315c38d9db7f3beeaa576a47e8f427ff52b481a9
Johnny Serup <serup@hotmail.com>
2016-10-28 05:51:53 +0200

    [nowrap] - install script issue
    Change-Id: I4d3fa089aaa9baf3a55d0631b7ea4d802b23de60

--- linux-install-nowrap.sh
+++ linux-install-nowrap.sh
@@ -1,5 +1,6 @@
-#/usr/bin bash
+#/usr/bin/env bash
 echo "Installing nowrap script for truncating color filled text from git log"
+mkdir -p ~/GitHub
 cd ~/GitHub
 git clone https://github.com/goodell/nowrap.git
 cd -
