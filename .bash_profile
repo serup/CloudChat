999b56322e3f906e15803bb7a5fd45fdef9a8b0d
Johnny Serup <serup@hotmail.com>
2016-10-28 22:07:39 +0200

    [tmux] - BUGFIX - resurrect issue
    Change-Id: I7622b8afb3435b592aab6bc800af5335ecfd9f0d

---
+++ .bash_profile
@@ -0,0 +1,7 @@
+# tmux-resurrect-ng prompt_runner for auto save/restore
+if [[ -n "$TMUX" ]]; then
+#	source "$HOME/.tmux/plugins/tmux-resurrect-ng/scripts/prompt_runner.sh"
+#	export PROMPT_COMMAND="tmxr_runner${PROMPT_COMMAND:+; }${PROMPT_COMMAND}"
+	source ~/.bashrc
+fi
+
--- linux-install-tmux-resurect.sh
+++ linux-install-tmux-resurect.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-echo "fetch tmux resurect plugin"
+echo "remove -if any- old tmux installation"
+sudo apt-get remove --auto-remove tmux
+sudo rm -rf ~/.tmux
+echo "fetch tmux and resurect plugin"
+sudo apt-get install tmux
 git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux
-echo "fetch tmux plugin manager"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo "add to .tmux.conf file"
-echo "run-shell ~/.tmux/ressurect.tmux" >> ~/.tmux.conf
+echo "copy .bash_profile and .tmux.conf files"
+cp .bash_profile ~/.
+cp .tmux.conf ~/.
+echo "-done -  start tmux, then write <prefix> I  -- this will install tmux plugins - NB! NOW RESTART PC inorder for resurrect to work " 
