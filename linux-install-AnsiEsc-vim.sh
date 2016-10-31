9045c3d6f436acb68e812ddbf638868b9e3c777c
Johnny Serup <serup@hotmail.com>
2016-10-31 17:15:55 +0100

    [install.sh] - issue with AnsiEsc
    Change-Id: I45dd7455c028042969c322f2e3fd682b866e8d77

--- linux-install-AnsiEsc-vim.sh
+++ linux-install-AnsiEsc-vim.sh
@@ -4,6 +4,10 @@ sudo git clone https://github.com/powerman/vim-plugin-AnsiEsc.git ~/.vim/bundle/
 echo "go into vim and write :helptags ~/.vim/"
 echo "then write :AnsiEsc"
 echo "now file will be showing color escape codes as color"
+echo " - if this does NOT work - then do following : "
+echo "sudo vim AnsiEsc.vba"
+echo ":so %"
+echo ":q"
 
 
 
