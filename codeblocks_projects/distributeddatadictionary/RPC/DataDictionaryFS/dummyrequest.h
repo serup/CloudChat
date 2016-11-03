8b3b8d8e463b98b40833509345d47b87a7c94eca
johnny serup <serup@hotmail.com>
2016-11-03 15:15:07 +0100

    [DDDfs_client] - autogen changes via makefile
    Change-Id: I27aa4e2284806bffdd548f88c34387e4b7d390d7

--- codeblocks_projects/distributeddatadictionary/RPC/DataDictionaryFS/dummyrequest.h
+++ codeblocks_projects/distributeddatadictionary/RPC/DataDictionaryFS/dummyrequest.h
@@ -17,3 +17,59 @@ DDRequest createDummyDDRequest()
 		return req;                                                                                   
 }                                                                                                 
 
+// Dummy handleResponse for testing purposes - example ./DDDfs_server (one terminal) ./DDDfs_client localhost (second terminal)
+bool handleResponse(std::unique_ptr<CDataEncoder> &decoder_ptr)
+{
+	bool bDecoded=false;
+	std::string strValue;
+
+	// decode data ...
+	if( DED_GET_STRUCT_START( decoder_ptr, "DDNodeResponse" ) == true )
+	{
+			if(	DED_GET_STDSTRING	( decoder_ptr, "message", strValue ) == true )
+			{
+					if( DED_GET_STRUCT_END( decoder_ptr, "DDNodeResponse" ) == true )
+					{
+							if(strValue == "Hello World") {
+									printf("\nResponse: OK\n"); 
+									bDecoded=true;
+							}
+							else {
+									printf("\nResponse: FAIL\n");                                 
+							}
+					}
+					else
+					{
+							bDecoded=false;
+							printf("\nResponse: FAIL\n");                                                                             
+					}	
+					cout << "message : " << strValue << endl;
+			}
+	}
+
+	if(!bDecoded)
+   	{ // expect specific protocol communication
+		if( DED_GET_STRUCT_START( decoder_ptr, "DDNodeResponse" ) == true )
+		{
+			std::string methodName="";
+			if(	DED_GET_METHOD	( decoder_ptr, "name", methodName ) == true )
+			{
+				cout << "OK: Response was method: " << methodName << endl;
+				//TODO: handle various responses from various requests
+				//
+				
+				bDecoded=true;
+			}
+			else	
+				cout << "FAIL: did NOT decode DDNodeResponse" << endl;
+		}
+
+		if(!bDecoded){
+		// could be server is sending a request
+			cout << "TODO: handle a possible Request from server" << endl;	
+			
+		}
+	}
+
+	return bDecoded;
+}
