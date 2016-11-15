5d4af2c99a4c2f0131a6a264e90fc6301f5c4d20
johnny serup <serup@hotmail.com>
2016-11-15 14:48:09 +0100

    [stout] - more dependencies
    Change-Id: I4ab30e18e828ba67de17efd7940e299b2ff1a567

--- linux-install-libprocess.sh
+++ linux-install-libprocess.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 echo "Clone libprocess library "
 cd ~/GitHub; git clone https://github.com/3rdparty/libprocess.git
+echo "- install into /usr/include/stout"
+cd ~/GitHub/libprocess; ./bootstrap
+cd ~/GitHub/libprocess; ./configure
+echo "- build stout"
+cd ~/GitHub/libprocess/3rdparty/stout; ./configure
+echo "- install into /usr/include/stout/"
+sudo cp -r ~/GitHub/libprocess/3rdparty/stout/include/stout/ /usr/include/
 echo "- install into /usr/include/process"
 sudo cp -r ~/GitHub/libprocess/include/process/ /usr/include/
 echo "- Done."
