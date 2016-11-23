3989f67d46c57baea0c0174561d594ba0dda255b
johnny serup <serup@hotmail.com>
2016-11-23 22:25:42 +0100

    [tmux] - install fix
    Change-Id: I74dfb461aef265acff85a4f7ed96e24e5a3e9321

--- INSTALLSCRIPTS/linux-install-tmux-resurect.sh
+++ INSTALLSCRIPTS/linux-install-tmux-resurect.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 echo "remove -if any- old tmux installation"
 sudo apt-get remove --auto-remove tmux
 sudo rm -rf ~/.tmux
@@ -7,7 +8,7 @@ sudo apt-get install tmux
 git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "copy .bash_profile and .tmux.conf files"
-cp .bash_profile ~/.
-cp .tmux.conf ~/.
-cp .bash_aliases ~/.
-echo "-done -  start tmux, then write <prefix> I  -- this will install tmux plugins - NB! NOW RESTART PC inorder for resurrect to work " 
+sudo cp ../.bash_profile ~/.
+sudo cp ../.tmux.conf ~/.
+sudo cp ../.bash_aliases ~/.
+echo "-done -  sudo start tmux, then write <prefix> I  -- this will install tmux plugins - NB! NOW RESTART PC inorder for resurrect to work " 
