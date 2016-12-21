3a09ba2acb973ea528bee4aac5017395ee148c36
johnny serup <serup@hotmail.com>
2016-12-21 10:30:21 +0100

    [python_projects] - add utils for image info
    Change-Id: I69a3c1be4acbba52d1252e2043fd730dec56fd8c

--- python_projects/dops/protocol/utils/utils.py
+++ python_projects/dops/protocol/utils/utils.py
@@ -3,6 +3,10 @@
 
 import sys
 from array import array
+import zlib, struct
+import StringIO
+import struct
+
 
 
 class CUtils:
@@ -102,7 +106,6 @@ class CUtils:
                     else:
                         output += "."
         return output
-
 
     def hexdump_to_string(self, chunkdata):
         length = len(chunkdata)
@@ -226,4 +229,93 @@ class CUtils:
         output += "</pre></html>"
         return output
 
+    def write_png(self, buf, width, height):
+        """ buf: must be bytes or a bytearray in Python3.x,
+            a regular string in Python2.x.
+        """
 
+        # reverse the vertical line order and add null bytes at the start
+        width_byte_4 = width * 4
+        raw_data = b''.join(b'\x00' + buf[span:span + width_byte_4]
+                        for span in range((height - 1) * width_byte_4, -1, - width_byte_4))
+
+        def png_pack(png_tag, data):
+            chunk_head = png_tag + data
+            return (struct.pack("!I", len(data)) +
+                chunk_head +
+                struct.pack("!I", 0xFFFFFFFF & zlib.crc32(chunk_head)))
+
+        return b''.join([
+        b'\x89PNG\r\n\x1a\n',
+        png_pack(b'IHDR', struct.pack("!2I5B", width, height, 8, 6, 0, 0, 0)),
+        png_pack(b'IDAT', zlib.compress(raw_data, 9)),
+        png_pack(b'IEND', b'')])
+
+# below is based on get_image_info.py from github Gist from Corey Goldberg, 2013
+# start of snippet
+    """validate and analyze dimensions of image files.  Supports: PNG, JPG, GIF."""
+    """example usage:
+            with open('foo.png', 'rb') as f:
+                data = f.read()
+            w, h = CUtils.get_image_dimensions(data)
+            print w, h
+    """
+
+    def is_gif(self, data):
+        return (data[:6] in ('GIF87a', 'GIF89a'))
+
+    def is_png(self, data):
+        return ((data[:8] == '\211PNG\r\n\032\n') and (data[12:16] == 'IHDR'))
+
+    def is_old_png(self, data):
+        return ((data[:8] == '\211PNG\r\n\032\n') and (data[12:16] != 'IHDR'))
+
+    def is_jpeg(self, data):
+        return (data[:2] == '\377\330')
+
+    def get_image_dimensions(self, data):
+        if len(data) < 16:
+            raise Exception('not valid image data')
+
+        elif self.is_gif(data):
+            w, h = struct.unpack('<HH', data[6:10])
+            width = int(w)
+            height = int(h)
+
+        elif self.is_png(data):
+            w, h = struct.unpack('>LL', data[16:24])
+            width = int(w)
+            height = int(h)
+
+        elif self.is_old_png(data):
+            w, h = struct.unpack('>LL', data[8:16])
+            width = int(w)
+            height = int(h)
+
+        elif self.is_jpeg(data):
+            jpeg = StringIO.StringIO(data)
+            jpeg.read(2)
+            b = jpeg.read(1)
+            try:
+                while (b and ord(b) != 0xDA):
+                    while (ord(b) != 0xFF): b = jpeg.read(1)
+                    while (ord(b) == 0xFF): b = jpeg.read(1)
+                    if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
+                        jpeg.read(3)
+                        h, w = struct.unpack('>HH', jpeg.read(4))
+                        break
+                    else:
+                        jpeg.read(int(struct.unpack('>H', jpeg.read(2))[0])-2)
+                    b = jpeg.read(1)
+                width = int(w)
+                height = int(h)
+            except struct.error:
+                pass
+            except ValueError:
+                pass
+
+        else:
+            raise Exception('not valid image data')
+
+        return width, height
+# end of snippet
