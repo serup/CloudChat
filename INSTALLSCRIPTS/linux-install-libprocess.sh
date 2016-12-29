56b06315ce2ff032fa81db15ec3e6ec4304cfe91
johnny serup <serup@hotmail.com>
2016-12-29 18:20:07 +0100

    [install] - libprocess, mesos -issues; wip
    Change-Id: I9e44dff2adefbe0575d13e2bc4ace3ba44c49e72

--- INSTALLSCRIPTS/linux-install-libprocess.sh
+++ INSTALLSCRIPTS/linux-install-libprocess.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-# DEPRECATED - MESOS project should be looked at instead - somehow this project fails and mesos as libprocess inside
+# TODO: Find a way to install all dependencies - MESOS project should be looked at instead - somehow this project fails and mesos as libprocess inside
 echo "Clone libprocess library "
 cd ~/GitHub; git clone https://github.com/3rdparty/libprocess.git
 echo "- install into /usr/include/stout"
 cd ~/GitHub/libprocess; ./bootstrap
 cd ~/GitHub/libprocess; ./configure
+cd ~/GitHub/libprocess; sudo make 
 echo "- build stout"
 cd ~/GitHub/libprocess/3rdparty/stout; ./configure
 echo "- install into /usr/include/stout/"
--- INSTALLSCRIPTS/linux-install-mesos.sh
+++ INSTALLSCRIPTS/linux-install-mesos.sh
@@ -2,6 +2,8 @@
 echo "Clone mesos library "
 #cd ~/GitHub; git clone https://github.com/apache/mesos.git
 cd ~/GitHub; git clone https://git-wip-us.apache.org/repos/asf/mesos.git
+echo "- install into /usr/include/stout - do NOT overwrite"
+sudo cp -ir ~/GitHub/mesos/3rdparty/stout/include/stout/ /usr/include/
 
 echo "- prereq - needed to build"
 # Install autotools (Only necessary if building from git repository).
@@ -16,12 +18,16 @@ mkdir -p ~/GitHub/mesos/build
 cd ~/GitHub/mesos/build; ../configure
 cd ~/GitHub/mesos/build; make
 
-#echo "- install into /usr/include/stout - do NOT overwrite"
-#sudo cp -ir ~/GitHub/mesos/3rdparty/stout/include/stout/ /usr/include/
-#echo "- copy static library libprocess to default /usr/lib"
-#sudo cp -ir ~/GitHub/mesos/3rdparty/libprocess/.libs/libprocess.a /usr/lib/
 echo "check build"
 cd ~/GitHub/mesos/build; make check
 echo "install mesos"
 cd ~/GitHub/mesos/build; sudo make install 
 echo "- Done."
+
+echo "build libproces"
+cd ~/GitHub/mesos/3rdparty/libprocess; sudo make
+
+echo "- copy static library libprocess to default /usr/lib"
+sudo cp -ir ~/GitHub/mesos/3rdparty/libprocess/.libs/libprocess.a /usr/lib/
+
+
