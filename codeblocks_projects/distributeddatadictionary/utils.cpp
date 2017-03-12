1f5fc23e806aa2f2d2001a15e5d97a30fc6f2255
johnny serup <serup@hotmail.com>
2017-03-12 15:11:54 +0100

    [utils] - bugfix; template does not have method declaration in .cpp file
    Change-Id: I2d7197dc3acad0f2fae5251ed43400b529999c52

--- codeblocks_projects/distributeddatadictionary/utils.cpp
+++ codeblocks_projects/distributeddatadictionary/utils.cpp
@@ -35,30 +35,30 @@ std::list<std::string> CUtils::handle_cmdline_input(std::string cmd)
 // Obviously an empty range doesn't require anything to happen, and a single-point range just requires calling f on the one and only value. 
 // For bigger ranges then an asynchronous task is spawned to handle the first half, and then the second half is handled by a recursive call.
 
-template<typename Iterator,typename Func>
-void CUtils::parallel_for_each(Iterator first,Iterator last,Func f)
-{
-	ptrdiff_t const range_length=last-first;
-	if(!range_length)
-		return;
-	if(range_length==1)
-	{
-		f(*first);
-		return;
-	}
-
-	Iterator const mid=first+(range_length/2);
-
-	std::future<void> bgtask=std::async(&parallel_for_each<Iterator,Func>, first,mid,f);
-	try
-	{
-		parallel_for_each(mid,last,f);
-	}
-	catch(...)
-	{
-		bgtask.wait();
-		throw;
-	}
-	bgtask.get();   
-}
+//template<typename Iterator,typename Func>
+//void CUtils::parallel_for_each(Iterator first,Iterator last,Func f)
+//{
+//	ptrdiff_t const range_length=last-first;
+//	if(!range_length)
+//		return;
+//	if(range_length==1)
+//	{
+//		f(*first);
+//		return;
+//	}
+//
+//	Iterator const mid=first+(range_length/2);
+//
+//	std::future<void> bgtask=std::async(&parallel_for_each<Iterator,Func>, first,mid,f);
+//	try
+//	{
+//		parallel_for_each(mid,last,f);
+//	}
+//	catch(...)
+//	{
+//		bgtask.wait();
+//		throw;
+//	}
+//	bgtask.get();   
+//}
 
