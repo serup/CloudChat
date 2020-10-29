384777d4bd97e2af93ffa2586332d5ac27391f5d
Johnny Serup <serup@hotmail.com>
2020-10-29 13:23:10 +0100

    add setup script for Vagrant access rights to windows virtualbox installation
    Change-Id: I38869f8d5d3189e8586dba2f2280332b4e141055

---
+++ setupVagrantForVirtualBoxOnWindows.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Setting up access rights for Vagrant to start VirtualBox entities outside WSL and inside virtualbox windows installation"
+
+echo "This must be run as source - like this :   .  ./<filename>.sh"
+
+export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
+export PATH="$PATH:/mnt/c/Programmer/Oracle/VirtualBox"
+export VAGRANT_WSL_WINDOWS_ACCESS_USER_HOME_PATH="/mnt/c/tmp/VagrantTestdir"
+export VAGRANT_DEFAULT_PROVIDER="virtualbox"
+export VAGRANT_WSL_WINDOWS_ACCESS_USER_HOME_PATH="/mnt/c/Users/serup"
